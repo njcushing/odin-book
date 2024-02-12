c337f5f6aa0570ff15a06230687b7afa12323915
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-12 13:53:51 +0000

    Tweak extendedPropTypes module functions for improved type safety

--- packages/frontend/src/utils/extendedPropTypes.ts
+++ packages/frontend/src/utils/extendedPropTypes.ts
@@ -15,7 +15,7 @@ function createRequiredValidator<T>(
     return requiredValidator;
 }
 
-function integerValidator(props: T, propName: string, componentName: string): Error | null {
+function integerValidator<T>(props: T, propName: string, componentName: string): Error | null {
     const value = props[propName as keyof typeof props];
     if (typeof value !== "number" || !Number.isInteger(value)) {
         return new Error(
@@ -28,12 +28,15 @@ export const integer = integerValidator;
 export const integerRequired = createRequiredValidator(integerValidator);
 
 function cssSizeValidator<T>(props: T, propName: string, componentName: string): Error | null {
-    const pattern =
-        /^(auto|fit-content|max-content|min-content)$|^\d+(\.\d+)?%$|^\d+(\.\d+)?(px|em|rem|vw|vh|vmin|vmax)?$/;
-    if (!pattern.test(props[propName as keyof typeof props])) {
-        return new Error(
-            `Invalid prop ${propName} supplied to ${componentName}. Expected a valid CSS size value.`,
-        );
+    const value = props[propName as keyof typeof props];
+    if (value) {
+        const pattern =
+            /^(auto|fit-content|max-content|min-content)$|^\d+(\.\d+)?%$|^\d+(\.\d+)?(px|em|rem|vw|vh|vmin|vmax)?$/;
+        if (value && !pattern.test(value)) {
+            return new Error(
+                `Invalid prop ${propName} supplied to ${componentName}. Expected a valid CSS size value.`,
+            );
+        }
     }
     return null;
 }
