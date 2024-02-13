d4b92cc0eb912af8feed12eb6e5dcc25bfc9a993
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-13 21:30:03 +0000

    Create boilerplate Post component and index file for imports

---
+++ packages/frontend/src/features/posts/Post/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./index.module.css";
+
+function Post() {
+    return (
+        <div className={styles["wrapper"]}>
+            <div className={styles["container"]}></div>
+        </div>
+    );
+}
+
+export default Post;
---
+++ packages/frontend/src/features/posts/index.ts
@@ -0,0 +1,7 @@
+import Post from "./Post";
+
+const Posts = {
+    Post,
+};
+
+export default Posts;
