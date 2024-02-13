ee48a09d4e6996700b19d7c0a7c49a440102d546
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-13 21:26:38 +0000

    Create basic responsive layout for Home page component

--- packages/frontend/src/pages/Home/index.tsx
+++ packages/frontend/src/pages/Home/index.tsx
@@ -1,73 +1,163 @@
 import LayoutUI from "@/layouts";
+import Navigation from "@/features/navigation";
 import styles from "./index.module.css";
 
 function Home() {
-    const nav = (
-        <div
-            style={{
-                width: "100%",
-                height: "100%",
-                backgroundColor: "rgb(100, 100, 100)",
-            }}
+    const main = null;
+    const otherContent = null;
+
+    const navOptionsWideStyles = { gap: "1rem" };
+    const navOptionsWide = [
+        <Navigation.Option
+            text="Home"
+            symbol="home"
+            link="/"
+            style={navOptionsWideStyles}
             key={0}
-        ></div>
-    );
-    const main = (
-        <div
-            style={{
+        />,
+        <Navigation.Option
+            text="Profile"
+            symbol="person"
+            link="/profile"
+            style={navOptionsWideStyles}
+            key={1}
+        />,
+        <Navigation.Option
+            text="Chats"
+            symbol="message"
+            link="/chats"
+            style={navOptionsWideStyles}
+            key={2}
+        />,
+    ];
+    const navOptionsThinStyles = { gap: "1rem", width: "auto" };
+    const navOptionsThin = [
+        <Navigation.Option symbol="home" link="/" style={navOptionsThinStyles} key={0} />,
+        <Navigation.Option symbol="person" link="/profile" style={navOptionsThinStyles} key={1} />,
+        <Navigation.Option symbol="message" link="/chats" style={navOptionsThinStyles} key={2} />,
+    ];
+
+    const navWide = (
+        <LayoutUI.List
+            label="navigation"
+            ordered={false}
+            listItems={navOptionsWide}
+            scrollable
+            listStyles={{
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                alignItems: "flex-start",
+                flexWrap: "nowrap",
+                gap: "0.2rem",
                 width: "100%",
-                height: "100%",
-                backgroundColor: "rgb(60, 60, 60)",
+                height: "auto",
+                padding: "0.3rem",
+                margin: "0rem",
             }}
-            key={0}
-        ></div>
+        />
     );
-    const otherContent = nav;
+
+    const navThin = (
+        <LayoutUI.List
+            label="navigation"
+            ordered={false}
+            listItems={navOptionsThin}
+            scrollable
+            listStyles={{
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                flexWrap: "nowrap",
+                gap: "0.2rem",
+                width: "auto",
+                height: "auto",
+                padding: "0.3rem",
+                margin: "0rem",
+            }}
+        />
+    );
 
     const layout = (
         <LayoutUI.Spatial
-            width="100%"
+            width="auto"
             height="100%"
             arrangements={[
                 {
                     type: "columns",
-                    width: "100%",
-                    minWidth: 960,
-                    maxWidth: 1440,
+                    width: "1200px",
+                    minWidth: 1200,
+                    maxWidth: 999999,
                     height: "100%",
                     minHeight: 0,
                     maxHeight: 999999,
                     areas: [
-                        { size: "25%", children: [nav] },
-                        { size: "50%", children: [main] },
-                        { size: "25%", children: [otherContent] },
+                        { size: "280px", children: [navWide] },
+                        { size: "600px", children: [main] },
+                        { size: "320px", children: [otherContent] },
                     ],
+                    justifySelf: "flex-start",
+                    alignSelf: "center",
                 },
                 {
                     type: "columns",
-                    width: "100%",
-                    minWidth: 480,
-                    maxWidth: 960,
+                    width: "980px",
+                    minWidth: 980,
+                    maxWidth: 1200,
                     height: "100%",
                     minHeight: 0,
                     maxHeight: 999999,
                     areas: [
-                        { size: "25%", children: [nav] },
-                        { size: "75%", children: [main] },
+                        { size: "60px", children: [navThin] },
+                        { size: "600px", children: [main] },
+                        { size: "320px", children: [otherContent] },
                     ],
+                    justifySelf: "flex-start",
+                    alignSelf: "center",
                 },
                 {
                     type: "columns",
-                    width: "100%",
+                    width: "660px",
+                    minWidth: 660,
+                    maxWidth: 980,
+                    height: "100%",
+                    minHeight: 0,
+                    maxHeight: 999999,
+                    areas: [
+                        { size: "60px", children: [navThin] },
+                        { size: "600px", children: [main] },
+                    ],
+                    justifySelf: "flex-start",
+                    alignSelf: "center",
+                },
+                {
+                    type: "columns",
+                    width: "360px",
+                    minWidth: 360,
+                    maxWidth: 660,
+                    height: "100%",
+                    minHeight: 0,
+                    maxHeight: 999999,
+                    areas: [
+                        { size: "60px", children: [navThin] },
+                        { size: "300px", children: [main] },
+                    ],
+                    justifySelf: "flex-start",
+                    alignSelf: "center",
+                },
+                {
+                    type: "columns",
+                    width: "320px",
                     minWidth: 0,
-                    maxWidth: 480,
+                    maxWidth: 360,
                     height: "100%",
                     minHeight: 0,
                     maxHeight: 999999,
                     areas: [
-                        { size: "70px", children: [nav] },
-                        { size: "auto", children: [main] },
+                        { size: "60px", children: [navThin] },
+                        { size: "260px", children: [main] },
                     ],
+                    justifySelf: "flex-start",
+                    alignSelf: "center",
                 },
             ]}
         />
