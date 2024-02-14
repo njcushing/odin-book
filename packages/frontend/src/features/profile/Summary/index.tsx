074201ff8c0d4e618a7184c190db28f22af5c5dc
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-14 22:43:48 +0000

    Write JSX for profile Summary component

--- packages/frontend/src/features/profile/Summary/index.tsx
+++ packages/frontend/src/features/profile/Summary/index.tsx
@@ -1,7 +1,62 @@
+import createMultilineTextTruncateStyles from "@/utils/createMultilineTextTruncateStyles";
+import ProfileImage from "@/components/ProfileImage";
 import styles from "./index.module.css";
 
-function Summary() {
-    return <div className={styles["container"]}></div>;
+type SummaryTypes = {
+    type: "wide" | "thin";
+};
+
+function Summary({ type }: SummaryTypes) {
+    if (type === "wide") {
+        return (
+            <div className={styles["container"]}>
+                <div className={styles["row-one"]}>
+                    <div className={styles["row-one-left"]}>
+                        <ProfileImage src={new Uint8Array([])} sizePx={64} />
+                    </div>
+                    <div className={styles["row-one-right"]}>
+                        <h3 className={`truncate-ellipsis ${styles["display-name"]}`}>
+                            John Smith is a Legendados
+                        </h3>
+                        <h4 className={`truncate-ellpisis ${styles["account-tag"]}`}>
+                            @JohnSmith84
+                        </h4>
+                    </div>
+                </div>
+                <div className={styles["row-two"]}>
+                    <p
+                        className={styles["bio"]}
+                        style={{ ...createMultilineTextTruncateStyles(4) }}
+                    >
+                        Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text
+                        Sample Text Sample Text Sample Text
+                    </p>
+                </div>
+                <div className={styles["row-three"]}>
+                    <h4 className={styles["following-count"]}>
+                        <strong>300</strong> Following
+                    </h4>
+                    <h4 className={styles["followers-count"]}>
+                        <strong>192</strong> Followers
+                    </h4>
+                    <h4 className={styles["likes-count"]}>
+                        <strong>3892</strong> Likes
+                    </h4>
+                </div>
+                <div className={styles["row-four"]}>
+                    <p className={styles["account-creation-date"]}>Joined February 2024</p>
+                </div>
+            </div>
+        );
+    }
+
+    if (type === "thin") {
+        return (
+            <div className={styles["container"]}>
+                <ProfileImage src={new Uint8Array([])} sizePx={48} />
+            </div>
+        );
+    }
 }
 
 export default Summary;
