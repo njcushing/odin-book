4abf7907e09411f9665cc68f495ecb78515500d1
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-14 20:45:57 +0000

    Convert List layout component to use TypeScript type instead of PropTypes

--- packages/frontend/src/layouts/List/index.tsx
+++ packages/frontend/src/layouts/List/index.tsx
@@ -1,96 +1,37 @@
-import PropTypes, { InferProps } from "prop-types";
-import * as extendedPropTypes from "@/utils/extendedPropTypes";
+import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import styles from "./index.module.css";
 
-const defaultStyles = {
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "flex-start",
-    flexWrap: "nowrap",
-    gap: "6px",
-    width: "100%",
-    height: "auto",
-    padding: "6px",
-    margin: "0px",
+type ListProps = {
+    label: string;
+    ordered?: boolean;
+    listItems: React.ReactNode[];
+    scrollable?: boolean;
+    listStyles?: React.CSSProperties;
 };
 
-function List({
-    label,
-    ordered,
-    listItems,
-    scrollable,
-    listStyles,
-}: InferProps<typeof List.propTypes>) {
+function List({ label, ordered, listItems, scrollable, listStyles }: ListProps) {
     const ListComponent = ordered ? "ol" : "ul";
 
     const items = listItems ? listItems.map((item) => ({ item, key: uuidv4() })) : [];
-
-    let stylesConcatenated = { ...defaultStyles };
-    if (listStyles) stylesConcatenated = { ...stylesConcatenated, ...listStyles };
 
     return (
         <div className={styles["wrapper"]}>
             <div className={styles["container"]}>
                 <div className={styles[`${scrollable ? "scrollable-wrapper" : ""}`]}>
-                    <ListComponent
-                        className={styles["list"]}
-                        aria-label={label}
-                        style={
-                            listStyles
-                                ? {
-                                      ...stylesConcatenated,
-                                      width: `calc(${stylesConcatenated.width} - (2 * ${stylesConcatenated.padding}))`,
-                                      height: `calc(${stylesConcatenated.height} - (2 * ${stylesConcatenated.padding}))`,
-                                  }
-                                : {}
-                        }
-                    >
-                        {items.map((item) => item.item)}
+                    <ListComponent className={styles["list"]} aria-label={label} style={listStyles}>
+                        {items.map((item) => {
+                            return (
+                                <li className={styles["list-item"]} key={item.key}>
+                                    {item.item}
+                                </li>
+                            );
+                        })}
                     </ListComponent>
                 </div>
             </div>
         </div>
     );
 }
-
-List.propTypes = {
-    label: PropTypes.string.isRequired,
-    ordered: PropTypes.bool,
-    listItems: PropTypes.arrayOf(PropTypes.element),
-    scrollable: PropTypes.bool,
-    listStyles: PropTypes.exact({
-        flexDirection: PropTypes.oneOf(["row", "column"]),
-        justifyContent: PropTypes.oneOf([
-            "flex-start",
-            "center",
-            "flex-end",
-            "space-between",
-            "space-around",
-            "space-evenly",
-        ]),
-        alignItems: PropTypes.oneOf([
-            "flex-start",
-            "center",
-            "flex-end",
-            "space-between",
-            "space-around",
-            "space-evenly",
-        ]),
-        flexWrap: PropTypes.oneOf(["nowrap", "wrap", "wrap-reverse"]),
-        gap: extendedPropTypes.cssSizeRequired,
-        width: extendedPropTypes.cssSizeRequired,
-        height: extendedPropTypes.cssSizeRequired,
-        padding: extendedPropTypes.cssSizeRequired,
-        margin: extendedPropTypes.cssSizeRequired,
-    }),
-};
-
-List.defaultProps = {
-    ordered: false,
-    listItems: [],
-    scrollable: true,
-    listStyles: { ...defaultStyles },
-};
 
 export default List;
