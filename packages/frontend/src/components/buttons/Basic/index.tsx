977640628a264b4fb38158e19bbff32a6295e2d0
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-17 12:16:35 +0000

    Add animations to Basic button component
    I also added the option to have a custom aria-label and to specify a child element for the component

--- packages/frontend/src/components/buttons/Basic/index.tsx
+++ packages/frontend/src/components/buttons/Basic/index.tsx
@@ -4,35 +4,42 @@ import styles from "./index.module.css";
 type BasicTypes = {
     type?: "button" | "reset" | "submit";
     text?: string;
+    label?: string;
     symbol?: string;
     onClickHandler?: ((event?: React.MouseEvent<HTMLButtonElement>) => void) | null;
     onSubmitHandler?: ((event?: React.FormEvent<HTMLButtonElement>) => void) | null;
     disabled?: boolean;
     palette?: "primary" | "secondary" | "bare" | "red" | "orange" | "green" | "blue";
+    animation?: "rigid" | "squishy";
     style?: {
         shape?: "sharp" | "rounded";
     };
     otherStyles?: React.CSSProperties;
+    children?: React.ReactElement | null;
 };
 
 function Basic({
     type = "button",
     text = "Button",
+    label = "",
     symbol = "",
     onClickHandler = null,
     onSubmitHandler = null,
     disabled = false,
     palette = "primary",
+    animation = "rigid",
     style = {
         shape: "rounded",
     },
     otherStyles = {},
+    children = null,
 }: BasicTypes) {
     return (
         <button
             // eslint-disable-next-line react/button-has-type
             type={type}
             className={styles["button"]}
+            aria-label={label}
             onClick={(e) => {
                 if (onClickHandler) onClickHandler(e);
                 e.currentTarget.blur();
@@ -53,11 +60,13 @@ function Basic({
             }}
             disabled={disabled}
             data-palette={palette}
+            data-animation={animation}
         >
             {symbol && symbol.length > 0 && (
                 <p className={`material-symbols-rounded ${styles["button-symbol"]}`}>{symbol}</p>
             )}
             {text}
+            {children}
         </button>
     );
 }
