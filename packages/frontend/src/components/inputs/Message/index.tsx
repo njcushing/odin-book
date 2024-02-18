4ca8cca7cf444309423b1820220b97e43dc22c90
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-18 13:06:59 +0000

    Write functionality for Message prefab input component
    Images can now be selected from the file system and their name and size will be displayed. They can be removed by clicking the 'X' displayed next to each one.

--- packages/frontend/src/components/inputs/Message/index.tsx
+++ packages/frontend/src/components/inputs/Message/index.tsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Buttons from "@/components/buttons";
+import validateField from "@/utils/validateField";
+import formatBytes from "@/utils/formatBytes";
 import styles from "./index.module.css";
 
 type Setter<T> = React.Dispatch<React.SetStateAction<T>>;
 
-type ImagesArray = number[][];
+type Images = {
+    [key: string]: {
+        image: number[];
+        file: File;
+    };
+};
 
 type Validator<T> = {
     func: (
@@ -37,7 +44,7 @@ type MessageTypes = {
     placeholder?: string;
     textValidator?: Validator<string>;
     imageValidator?: Validator<number[]>;
-    submitHandler?: Submitter<string, ImagesArray>;
+    submitHandler?: Submitter<string, Images>;
     submissionErrors?: string[];
     sending?: boolean;
 };
@@ -53,72 +60,105 @@ function Message({
 }: MessageTypes) {
     const [textareaContent, setTextareaContent]: [string, Setter<string>] = useState<string>(text);
     const [textError, setTextError]: [string, Setter<string>] = useState<string>("");
-    const [images, setImages]: [ImagesArray, Setter<ImagesArray>] = useState<ImagesArray>([]);
+    const [images, setImages]: [Images, Setter<Images>] = useState<Images>({});
     const [imageError, setImageError]: [string, Setter<string>] = useState<string>("");
+
+    const createImageButton = (key: string, file: { name: string }) => {
+        return (
+            <li className={styles["image-button"]} aria-label="image to load" key={key}>
+                <div className={styles["file-info"]}>
+                    <p className={styles["image-file-name"]}>{file.name}</p>
+                    <p className={styles["image-file-size"]}>{formatBytes(file.size, 2)}</p>
+                </div>
+                <Buttons.Basic
+                    text=""
+                    symbol="cancel"
+                    onClickHandler={() => {
+                        const newImages = { ...images };
+                        delete newImages[key];
+                        setImages(newImages);
+                    }}
+                    otherStyles={{ fontSize: "1.0rem", padding: "0.2rem" }}
+                />
+            </li>
+        );
+    };
 
     return (
         <>
             <div className={styles["message-box"]} aria-label="message-box">
-                <textarea
-                    className={styles["message-textarea"]}
-                    aria-label="message-textarea"
-                    id="text"
-                    defaultValue={textareaContent}
-                    placeholder={placeholder}
-                    required
-                    style={{
-                        resize: "none",
-                    }}
-                    onChange={(e) => {
-                        let valid = true;
-                        let error = "";
-                        if (textValidator && textValidator.func) {
-                            const result = textValidator.func(e.target.value, "front");
-                            if (!result.status) {
-                                valid = false;
-                                error = result.message || "";
-                            }
-                        }
-                        if (!valid) {
-                            setTextError(error);
-                        }
-                        setTextareaContent(e.target.value);
-                    }}
-                    disabled={sending}
-                ></textarea>
+                <div className={styles["input-container"]}>
+                    <textarea
+                        className={styles["message-textarea"]}
+                        aria-label="message-textarea"
+                        id="text"
+                        defaultValue={textareaContent}
+                        placeholder={placeholder}
+                        required
+                        style={{
+                            resize: "none",
+                        }}
+                        onChange={(e) => {
+                            const valid = validateField(
+                                e.target.value,
+                                textValidator || null,
+                                false,
+                            );
+                            if (!valid.status)
+                                setTextError(valid.message || "Something went wrong.");
+                            setTextareaContent(e.target.value);
+                        }}
+                        disabled={sending}
+                    ></textarea>
+                    {Object.keys(images).length > 0 ? (
+                        <ul className={styles["image-buttons-list"]}>
+                            {Object.keys(images).map((key) =>
+                                createImageButton(key, images[key].file),
+                            )}
+                        </ul>
+                    ) : null}
+                </div>
                 <div className={styles["send-options-container"]}>
                     <Buttons.Upload
-                        accept="image/*"
-                        multiple={false}
+                        accept="image.*"
+                        multiple
                         button={{
                             text: "",
                             symbol: "image",
                             palette: "primary",
                             style: { shape: "rounded" },
                         }}
-                        onUploadHandler={(e) => {
-                            if (
-                                /* Ensure file being loaded is of type 'ArrayBufferLike' */
-                                e.target &&
-                                e.target.result &&
-                                typeof e.target.result !== "string"
-                            ) {
-                                const imgArray = Array.from(new Uint8Array(e.target.result));
-                                let valid = true;
-                                let error = "";
-                                if (imageValidator && imageValidator.func) {
-                                    const result = imageValidator.func(imgArray, "front");
-                                    if (!result.status) {
-                                        valid = false;
-                                        error = result.message || "";
+                        onUploadHandler={(uploads: [ProgressEvent<FileReader>, File][]) => {
+                            const newImages: Images = {};
+                            uploads.forEach((upload) => {
+                                const [event, file] = upload;
+                                if (!file.type.match("image.*")) return;
+                                if (
+                                    /* Ensure file being loaded is of type 'ArrayBufferLike' */
+                                    event.target &&
+                                    event.target.result &&
+                                    typeof event.target.result !== "string"
+                                ) {
+                                    const imgArray = Array.from(
+                                        new Uint8Array(event.target.result),
+                                    );
+                                    const valid = validateField(
+                                        imgArray,
+                                        imageValidator || null,
+                                        false,
+                                    );
+                                    if (!valid.status) {
+                                        setImageError(valid.message || "Something went wrong.");
+                                    } else {
+                                        const key = uuidv4();
+                                        newImages[key] = {
+                                            image: imgArray,
+                                            file,
+                                        };
                                     }
                                 }
-                                if (valid) {
-                                    setImages([...images, imgArray]);
-                                } else {
-                                    setImageError(error);
-                                }
-                            }
+                            });
+                            setImages({ ...images, ...newImages });
                         }}
                     />
                     <Buttons.Basic
