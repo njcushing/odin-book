fcb5666c4c3b8bd0bab8e278ea0bc8e68bae3e65
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-19 21:46:46 +0000

    Move chat header to its own component: 'Header'
    I have also added the functionality for changing the chat name; the 'onEditNameHandler' callback function provided as a prop will only be invoked if the chat's name has changed (just opening the edit input element by clicking the button and closing it again will not invoke the function).

---
+++ packages/frontend/src/features/chat/Header/index.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import * as extendedTypes from "@/utils/extendedTypes";
+import Buttons from "@/components/buttons";
+import styles from "./index.module.css";
+
+const buttonStyles = { fontSize: "1.1rem", padding: "0.6rem" };
+
+type HeaderTypes = {
+    name?: string;
+    onEditNameHandler?: ((name: string) => void) | null;
+};
+
+function Header({ name = "Chat Title", onEditNameHandler = null }: HeaderTypes) {
+    const [editingName, setEditingName]: [boolean, extendedTypes.Setter<boolean>] = useState(false);
+    const [chatName, setChatName]: [string, extendedTypes.Setter<string>] = useState(name);
+    const [nameStored, setNameStored]: [string, extendedTypes.Setter<string>] = useState(name);
+
+    useEffect(() => {
+        if (!editingName && onEditNameHandler && chatName !== nameStored) {
+            onEditNameHandler(chatName);
+            setNameStored(chatName);
+        }
+    }, [editingName, chatName, onEditNameHandler, nameStored]);
+
+    return (
+        <div className={styles["header"]}>
+            <div className={styles["name-container"]}>
+                {!editingName ? (
+                    <h3 className={`truncate-ellipsis ${styles["name"]}`}>{chatName}</h3>
+                ) : (
+                    <input
+                        className={styles["edit-name"]}
+                        type="text"
+                        defaultValue={chatName}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                            setChatName(e.target.value)
+                        }
+                    ></input>
+                )}
+                <Buttons.Basic
+                    text=""
+                    symbol="edit"
+                    label="edit chat name"
+                    otherStyles={{ ...buttonStyles }}
+                    onClickHandler={() => {
+                        setEditingName(!editingName);
+                    }}
+                />
+            </div>
+            <div className={styles["options"]}>
+                <Buttons.Basic
+                    text=""
+                    symbol="person_add"
+                    label="add people"
+                    palette="blue"
+                    otherStyles={{ ...buttonStyles }}
+                />
+                <Buttons.Basic
+                    text=""
+                    symbol="call"
+                    label="call"
+                    palette="blue"
+                    otherStyles={{ ...buttonStyles }}
+                />
+                <Buttons.Basic
+                    text=""
+                    symbol="videocam"
+                    label="call with video"
+                    palette="blue"
+                    otherStyles={{ ...buttonStyles }}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Header;
--- packages/frontend/src/features/chat/index.ts
+++ packages/frontend/src/features/chat/index.ts
@@ -1,9 +1,11 @@
 import Panel from "./Panel";
 import Message from "./Message";
+import Header from "./Header";
 
 const Chat = {
     Panel,
     Message,
+    Header,
 };
 
 export default Chat;
