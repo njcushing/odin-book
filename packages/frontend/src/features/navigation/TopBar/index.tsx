0c383c39bde16e97082ecfe8c2df67d72c157580
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-20 18:55:50 +0000

    Create boilerplate for TopBar navigation component

---
+++ packages/frontend/src/features/navigation/TopBar/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./index.module.css";
+
+function TopBar() {
+    return <div className={styles["container"]}></div>;
+}
+
+export default TopBar;
--- packages/frontend/src/features/navigation/index.ts
+++ packages/frontend/src/features/navigation/index.ts
@@ -1,8 +1,10 @@
 import Menu from "./Menu";
+import TopBar from "./TopBar";
 import Option from "./Option";
 
 const Navigation = {
     Menu,
+    TopBar,
     Option,
 };
 
