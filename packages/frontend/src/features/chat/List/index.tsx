de5407752d625b6cd13f5178692212744f3e514c
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-21 21:40:33 +0000

    Write basic functionality and style rules for chat List component

--- packages/frontend/src/features/chat/List/index.tsx
+++ packages/frontend/src/features/chat/List/index.tsx
@@ -1,5 +1,86 @@
+import LayoutUI from "@/layouts";
+import Buttons from "@/components/buttons";
+import Chat from "..";
 import styles from "./index.module.css";
 
-function List() {}
+function List() {
+    const chats = [null, null, null, null, null, null, null, null, null, null, null, null, null];
+
+    const buttons = (
+        <div className={styles["create-new-chat-button-container"]} key={0}>
+            <Buttons.Basic
+                text="New"
+                symbol="add"
+                palette="green"
+                otherStyles={{
+                    fontSize: "1.15rem",
+                    padding: "0.8rem 1.6rem",
+                }}
+            />
+        </div>
+    );
+
+    const chatsList = (
+        <LayoutUI.List
+            label="navigation"
+            ordered={false}
+            listItems={[
+                chats.map((chat, i) => {
+                    return (
+                        <Chat.Option
+                            name=""
+                            participants={["name", "name", "name", "name", "name", "name", "name"]}
+                            image={{ src: new Uint8Array([]), alt: "" }}
+                            recentMessage={{ author: "test", text: "test" }}
+                            key={i}
+                        />
+                    );
+                }),
+            ]}
+            scrollable
+            listStyles={{
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                flexWrap: "nowrap",
+                gap: "0.2rem",
+                width: "calc(100% - (2 * 0.4rem))",
+                height: "auto",
+                padding: "0.4rem",
+                margin: "0rem",
+            }}
+            key={0}
+        />
+    );
+
+    return (
+        <div className={styles["container"]}>
+            <LayoutUI.Spatial
+                width="100%"
+                height="100%"
+                arrangements={[
+                    {
+                        type: "rows",
+                        minWidth: 0,
+                        maxWidth: 999999,
+                        minHeight: 0,
+                        maxHeight: 999999,
+                        areas: [
+                            { size: "auto", children: [buttons] },
+                            { size: "1fr", children: [chatsList] },
+                        ],
+                        style: {
+                            justifySelf: "flex-start",
+                            alignSelf: "center",
+                            width: "100%",
+                            height: "100%",
+                            padding: "0rem",
+                        },
+                    },
+                ]}
+            />
+        </div>
+    );
+}
 
 export default List;
