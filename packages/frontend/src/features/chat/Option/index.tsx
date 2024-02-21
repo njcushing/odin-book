818fcf9d43053e338714cbb53827b75ccb85cbae
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-21 21:49:49 +0000

    Add 'Delete' button to chat Option component

--- packages/frontend/src/features/chat/Option/index.tsx
+++ packages/frontend/src/features/chat/Option/index.tsx
@@ -1,4 +1,6 @@
 import Images from "@/components/images";
+import Buttons from "@/components/buttons";
+import combineParticipantNames from "../utils/combineParticipantNames";
 import styles from "./index.module.css";
 
 type RecentMessageTypes = {
@@ -11,9 +13,23 @@ type OptionTypes = {
     participants?: string[];
     image?: Images.Types.Profile;
     recentMessage?: RecentMessageTypes;
+    onDeleteClickHandler?: ((event: React.MouseEvent<HTMLButtonElement>) => void) | null;
 };
 
-function Option({ name = "", participants = [], image, recentMessage }: OptionTypes) {
+function Option({
+    name = "",
+    participants = [],
+    image,
+    recentMessage,
+    onDeleteClickHandler = null,
+}: OptionTypes) {
+    let chatName = "Chat";
+    if (name && name.length > 0) {
+        chatName = name;
+    } else if (participants && participants.length > 0) {
+        chatName = combineParticipantNames(participants, 3);
+    }
+
     return (
         <div className={styles["container"]}>
             <div className={styles["profile-image-container"]}>
@@ -25,11 +41,9 @@ function Option({ name = "", participants = [], image, recentMessage }: OptionTy
                 />
             </div>
             <div className={styles["name-and-message-container"]}>
-                {name && name.length > 0 ? (
-                    <p className={`truncate-ellpisis ${styles["name"]}`} aria-label="chat name">
-                        {name}
-                    </p>
-                ) : null}
+                <p className={`truncate-ellipsis ${styles["name"]}`} aria-label="chat name">
+                    {chatName}
+                </p>
                 {recentMessage ? (
                     <p
                         className={`truncate-ellipsis ${styles["message"]}`}
@@ -39,6 +53,18 @@ function Option({ name = "", participants = [], image, recentMessage }: OptionTy
                     </p>
                 ) : null}
             </div>
+            <Buttons.Basic
+                text=""
+                symbol="delete"
+                onClickHandler={(e) => {
+                    if (onDeleteClickHandler) onDeleteClickHandler(e);
+                }}
+                palette="red"
+                otherStyles={{
+                    fontSize: "0.9rem",
+                    padding: "0.5rem",
+                }}
+            />
         </div>
     );
 }
