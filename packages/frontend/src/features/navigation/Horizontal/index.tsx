1b72ff649482f4c5c4bea8d67405b8cad7a230cc
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-22 13:38:19 +0000

    Add drop-down menu to Horizontal navigation component
    This menu contains all the options that cannot be rendered in the available horizontal space on the normal navigation bar.

--- packages/frontend/src/features/navigation/Horizontal/index.tsx
+++ packages/frontend/src/features/navigation/Horizontal/index.tsx
@@ -23,20 +23,27 @@ function Horizontal({
 }: HorizontalTypes) {
     const [optionSelected, setOptionSelected] = useState<string | null>(selected);
     const [optionsDisplaying, setOptionsDisplaying] = useState<Option[]>(options);
+    const [optionsInMenu, setOptionsInMenu] = useState<Option[]>([]);
     const [displayMenuButton, setDisplayMenuButton] = useState<boolean>(false);
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
-    const listRef = useRef(null);
-    const menuButtonRef = useRef(null);
+    const listRef = useRef<HTMLUListElement>(null);
+    const hiddenMenuButtonContainerRef = useRef<HTMLDivElement>(null);
+    const menuButtonContainerRef = useRef<HTMLDivElement>(null);
 
     const fontSize = style.fontSize || "1.1rem";
+
+    const selectedOptionVisible =
+        optionSelected && optionsInMenu.map((option) => option.text).indexOf(optionSelected) === -1;
 
     const menuButton = (
         <button
             type="button"
             className={styles["option"]}
             aria-label="more options"
-            data-highlighted={false}
-            ref={menuButtonRef}
+            data-selected={menuOpen}
+            data-highlighted={!selectedOptionVisible}
+            onClick={() => setMenuOpen(!menuOpen)}
         >
             <p
                 className={`material-symbols-rounded ${styles["symbol"]}`}
@@ -78,7 +85,9 @@ function Horizontal({
              *  if there are too many options to fit in the available space, see if the menu button
              *  can fit in the remaining space. If not, remove the last option to make room.
              */
-            const menuButtonWidth = 0;
+            const menuButtonWidth = hiddenMenuButtonContainerRef.current
+                ? hiddenMenuButtonContainerRef.current.clientWidth
+                : 0;
             if (optionsToDisplay.length < options.length) {
                 if (currentWidthTotal + menuButtonWidth > maximumWidth) {
                     optionsToDisplay.pop();
@@ -87,8 +96,13 @@ function Horizontal({
             } else {
                 setDisplayMenuButton(false);
             }
-
             setOptionsDisplaying(optionsToDisplay);
+
+            const optionsToHide: Option[] = [];
+            for (let i = optionsToDisplay.length; i < options.length; i++) {
+                optionsToHide.push(options[i]);
+            }
+            setOptionsInMenu(optionsToHide);
         });
         if (listRef.current) {
             listRefCurrent = listRef.current;
@@ -138,9 +152,59 @@ function Horizontal({
                         </button>
                     );
                 })}
-                {displayMenuButton ? menuButton : null}
+                {displayMenuButton ? <div ref={menuButtonContainerRef}>{menuButton}</div> : null}
             </ul>
-            <div className={styles["menu-button-false-render"]}>{menuButton}</div>
+            {menuOpen ? (
+                <ul
+                    className={styles["menu"]}
+                    style={
+                        menuButtonContainerRef.current
+                            ? {
+                                  top:
+                                      menuButtonContainerRef.current.offsetTop +
+                                      menuButtonContainerRef.current.offsetHeight +
+                                      4,
+                                  right: 4,
+                              }
+                            : { top: 4, right: 4 }
+                    }
+                >
+                    {optionsInMenu.map((option) => {
+                        return (
+                            <button
+                                type="button"
+                                className={styles["option"]}
+                                data-highlighted={!!(optionSelected === option.text)}
+                                onClick={() => {
+                                    setOptionSelected(option.text);
+                                    setMenuOpen(false);
+                                }}
+                                disabled={option.disabled && option.disabled}
+                                key={option.text}
+                            >
+                                <div
+                                    className={styles["text"]}
+                                    style={{
+                                        fontSize,
+                                        paddingLeft: minPaddingPx,
+                                        paddingRight: minPaddingPx,
+                                    }}
+                                >
+                                    {option.text}
+                                </div>
+                                <div className={styles["highlight-bar"]}></div>
+                            </button>
+                        );
+                    })}
+                </ul>
+            ) : null}
+            <div
+                className={styles["menu-button-false-render"]}
+                hidden
+                ref={hiddenMenuButtonContainerRef}
+            >
+                {menuButton}
+            </div>
         </nav>
     );
 }
