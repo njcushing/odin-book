9a6b3c8d95d67016ce19afb6008fe0cdcd45554b
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-22 09:51:53 +0000

    Update Basic image component
    Setting width and height CSS rules for the component can be done through the 'style' prop - using purely pixels for dimensions was too restrictive (e.g. - if 100% was the desired width).

--- packages/frontend/src/components/images/Basic/index.tsx
+++ packages/frontend/src/components/images/Basic/index.tsx
@@ -2,27 +2,18 @@ import objectURLFromTypedArray from "@/utils/objectURLFromTypedArray";
 import * as Types from "../types";
 import styles from "./index.module.css";
 
-function Basic({
-    src = new Uint8Array([]),
-    alt = "",
-    label = "image",
-    size = { widthPx: 100, heightPx: 50 },
-    style,
-}: Types.Basic) {
+function Basic({ src = new Uint8Array([]), alt = "", label = "image", style }: Types.Basic) {
     let imgSrc = "";
     if (src) imgSrc = objectURLFromTypedArray(src);
 
     return (
-        <div className={styles["container"]} style={{ ...style }}>
+        <div className={styles["container"]}>
             <img
                 className={styles["image"]}
                 aria-label={label}
                 src={imgSrc}
                 alt={alt}
-                style={{
-                    height: `${size && size.heightPx}px`,
-                    width: `${size && size.widthPx}px`,
-                }}
+                style={{ ...style }}
             ></img>
         </div>
     );
--- packages/frontend/src/components/images/types.ts
+++ packages/frontend/src/components/images/types.ts
@@ -1,10 +1,5 @@
 import * as extendedTypes from "@/utils/extendedTypes";
 
-type Sizes2D = {
-    widthPx?: number;
-    heightPx?: number;
-};
-
 type Base = {
     src?: extendedTypes.TypedArray;
     alt?: string;
@@ -12,9 +7,7 @@ type Base = {
     style?: React.CSSProperties;
 };
 
-export type Basic = Base & {
-    size?: Sizes2D;
-};
+export type Basic = Base;
 
 export type Profile = Base & {
     status?: "online" | "away" | "busy" | "offline" | null;
