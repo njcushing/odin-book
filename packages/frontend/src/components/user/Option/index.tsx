3177309f7f1679af965e8a1cad558d82a4fa0c3b
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-23 11:04:38 +0000

    Write functionality and style rules for user Option component

--- packages/frontend/src/components/user/Option/index.tsx
+++ packages/frontend/src/components/user/Option/index.tsx
@@ -1,7 +1,34 @@
+import Buttons from "@/components/buttons";
+import User from "..";
+import * as Types from "../types";
 import styles from "./index.module.css";
 
-function Option() {
-    return <div className={styles["container"]}></div>;
+function Option({ user, following }: Types.Option) {
+    return (
+        <div className={styles["container"]}>
+            <User.ImageAndName
+                image={{ src: user.image.src, alt: user.image.alt }}
+                displayName={user.displayName}
+                accountTag={user.accountTag}
+                size="s"
+            />
+            {following ? (
+                <Buttons.Basic
+                    text="Unfollow"
+                    symbol="person_remove"
+                    palette="red"
+                    otherStyles={{ fontSize: "1.0rem" }}
+                />
+            ) : (
+                <Buttons.Basic
+                    text="Follow"
+                    symbol="person_add"
+                    palette="orange"
+                    otherStyles={{ fontSize: "1.0rem" }}
+                />
+            )}
+        </div>
+    );
 }
 
 export default Option;
