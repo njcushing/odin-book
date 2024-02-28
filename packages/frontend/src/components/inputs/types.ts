02f4b96e2519752314d3eccd37aa437052401134
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-28 21:14:56 +0000

    Create types file for input-based components

---
+++ packages/frontend/src/components/inputs/types.ts
@@ -0,0 +1,29 @@
+import * as validation from "./utils/validation";
+
+export type Base = {
+    labelText: string;
+    fieldId: string;
+    fieldName: string;
+    initialValue?: string | null;
+    onChangeHandler?: ((event: React.ChangeEvent<HTMLInputElement>) => void) | null;
+    disabled?: boolean;
+    readOnly?: boolean;
+    required?: boolean;
+};
+
+export type Placeholder = {
+    placeholder?: string | null;
+};
+
+export type Error = {
+    errorMessage?: string | null;
+    displayErrorOnMount?: boolean;
+};
+
+export type Validator<T> = {
+    validator?: validation.Validator<T>;
+};
+
+export type Sizes = {
+    size?: "xs" | "s" | "m" | "l" | "xl";
+};
