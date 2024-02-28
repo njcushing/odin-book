04a18bbf26184ea15f2b3053cc84aaaaba54014e
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-28 20:52:01 +0000

    Create Validator type and validate function for input-based components

---
+++ packages/frontend/src/components/inputs/utils/validation.ts
@@ -0,0 +1,27 @@
+export type Validator<T> = {
+    func: (
+        value: T,
+        messageType: "front" | "back",
+        ...args: unknown[]
+    ) => {
+        status: boolean;
+        message: string | null;
+    };
+    args?: unknown[];
+} | null;
+
+export function validate<T>(
+    fieldValue: T,
+    validator: Validator<T>,
+    required: boolean,
+): { status: boolean; message: string | null } {
+    const args = (validator && validator.args) || [];
+    const validValue =
+        validator && validator.func
+            ? validator.func(fieldValue, "front", args)
+            : { status: true, message: null };
+    if (!required && (!fieldValue || fieldValue === "")) {
+        validValue.status = true;
+    }
+    return validValue;
+}
