e1301d50d671edf06168ad6ebb82015e024f7431
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-28 21:29:23 +0000

    Create Text input component and update index file for exports

--- packages/frontend/src/components/inputs/index.ts
+++ packages/frontend/src/components/inputs/index.ts
@@ -1,12 +1,14 @@
 import Label from "./components/Label";
 import Description from "./components/Description";
 import Error from "./components/Error";
+import Text from "./components/Text";
 import Message from "./components/Message";
 
 const Inputs = {
     Message,
     Label,
     Description,
+    Text,
     Error,
 };
 
