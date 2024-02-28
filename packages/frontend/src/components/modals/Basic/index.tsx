d4fe6d536c3694bf262c22fabdab7bc512d7a3ab
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-28 20:31:32 +0000

    Write basic functionality and style rules for Basic modal component

--- packages/frontend/src/components/modals/Basic/index.tsx
+++ packages/frontend/src/components/modals/Basic/index.tsx
@@ -1,7 +1,32 @@
+import React from "react";
+import Buttons from "@/components/buttons";
 import styles from "./index.module.css";
 
-function Basic() {
-    return <div className={styles["container"]}></div>;
+type BasicTypes = {
+    fill: "page" | "parent";
+    onCloseClickHandler?: ((event: React.MouseEvent<HTMLButtonElement>) => void) | null;
+    style?: React.CSSProperties;
+};
+
+function Basic({ fill, onCloseClickHandler = null, style = {} }: BasicTypes) {
+    return (
+        <div className={styles["container"]}>
+            <div className={styles["modal"]} style={{ ...style }}>
+                <div className={styles["close-button-container"]}>
+                    <Buttons.Basic
+                        type="button"
+                        text=""
+                        symbol="close"
+                        label="close"
+                        onClickHandler={onCloseClickHandler}
+                        style={{ shape: "rounded" }}
+                        palette="primary"
+                        otherStyles={{ fontSize: "1.2rem", padding: "0.3rem" }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Basic;
