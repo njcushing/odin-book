e8f7c3d3f798c3c938261415eb96cfd9baef1aff
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-29 09:35:20 +0000

    Create Password input component and update index file for exports

--- packages/frontend/src/components/inputs/index.ts
+++ packages/frontend/src/components/inputs/index.ts
@@ -2,14 +2,16 @@ import Label from "./components/Label";
 import Description from "./components/Description";
 import Error from "./components/Error";
 import Text from "./components/Text";
+import Password from "./components/Password";
 import Message from "./components/Message";
 
 const Inputs = {
-    Message,
     Label,
     Description,
-    Text,
     Error,
+    Text,
+    Password,
+    Message,
 };
 
 export default Inputs;
