e408b159de3a377d6e1b53c528d26e9e8427a053
njcushing <129661299+njcushing@users.noreply.github.com>
2024-02-29 10:33:41 +0000

    Create Email input component and update index file for exports

---
+++ packages/frontend/src/components/inputs/components/Email/index.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import * as extendedTypes from "@/utils/extendedTypes";
+import * as Types from "../../types";
+import * as validation from "../../utils/validation";
+import getSizes from "../../utils/getSizes";
+import Inputs from "../..";
+import styles from "./index.module.css";
+
+type Custom = { description?: string };
+
+type EmailTypes = Types.Base &
+    Types.Placeholder &
+    Types.Error &
+    Types.Validator<string> &
+    Types.Sizes &
+    Custom;
+
+function Email({
+    labelText,
+    fieldId,
+    fieldName,
+    initialValue = "",
+    onChangeHandler = null,
+    disabled = false,
+    readOnly = false,
+    required = false,
+    placeholder = "",
+    errorMessage = "",
+    validator = null,
+    size = "s",
+    description = "",
+}: EmailTypes) {
+    const [value, setValue]: [string, extendedTypes.Setter<string>] = useState(initialValue || "");
+    const [error, setError]: [string, extendedTypes.Setter<string>] = useState("");
+
+    const sizes = getSizes(size, "input");
+
+    let currentErrorMessage = "";
+    if (errorMessage && errorMessage.length > 0) {
+        currentErrorMessage = errorMessage;
+    } else if (error && error.length > 0) {
+        currentErrorMessage = error;
+    }
+
+    return (
+        <div className={styles["wrapper"]}>
+            <Inputs.Label labelText={labelText} fieldId={fieldId} required={required} size={size} />
+            <Inputs.Description text={description} size={size} />
+            <input
+                className={styles[`input${currentErrorMessage.length > 0 ? "-error" : ""}`]}
+                type="email"
+                id={fieldId}
+                name={fieldName}
+                defaultValue={value}
+                style={{
+                    resize: "none",
+                    ...sizes,
+                }}
+                onChange={(e) => {
+                    const validValue = validation.validate(
+                        e.target.value,
+                        validator || validator,
+                        required,
+                    );
+                    setValue(e.target.value);
+                    setError(!validValue.status && validValue.message ? validValue.message : "");
+                    if (onChangeHandler) onChangeHandler(e);
+                }}
+                disabled={disabled || false}
+                readOnly={readOnly}
+                placeholder={placeholder || ""}
+            ></input>
+            {!disabled && currentErrorMessage.length > 0 ? (
+                <Inputs.Error text={currentErrorMessage} size={size} />
+            ) : null}
+        </div>
+    );
+}
+
+export default Email;
--- packages/frontend/src/components/inputs/index.ts
+++ packages/frontend/src/components/inputs/index.ts
@@ -3,6 +3,7 @@ import Description from "./components/Description";
 import Error from "./components/Error";
 import Text from "./components/Text";
 import Password from "./components/Password";
+import Email from "./components/Email";
 import Message from "./components/Message";
 
 const Inputs = {
@@ -11,6 +12,7 @@ const Inputs = {
     Error,
     Text,
     Password,
+    Email,
     Message,
 };
 
