5821368491f0e3f7cd6de04d40fc5a11cca6b029
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-01 11:14:04 +0000

    Remove Types from image components' index export file

--- packages/frontend/src/components/images/index.ts
+++ packages/frontend/src/components/images/index.ts
@@ -1,11 +1,9 @@
 import Basic from "./components/Basic";
 import Profile from "./components/Profile";
-import * as Types from "./types";
 
 const Images = {
     Basic,
     Profile,
-    Types,
 };
 
 export default Images;
