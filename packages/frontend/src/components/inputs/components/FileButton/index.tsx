30aef11b05c67599af2a3850df3a8b72f8f2bd52
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-01 12:58:59 +0000

    Add 'size' prop to FileButton component
    The component will now import its sizes from getSizes.

--- packages/frontend/src/components/inputs/components/FileButton/index.tsx
+++ packages/frontend/src/components/inputs/components/FileButton/index.tsx
@@ -1,20 +1,34 @@
 import Buttons from "@/components/buttons";
 import formatBytes from "@/utils/formatBytes";
 import React from "react";
+import * as Types from "../../types";
+import getSizes from "../../utils/getSizes";
 import styles from "./index.module.css";
 
-type FileButtonTypes = {
+type FileButtonTypes = Types.Sizes & {
     file: File;
     label?: string;
     onClickHandler: ((event: React.MouseEvent<HTMLButtonElement>) => void) | null;
 };
 
-function FileButton({ file, label = "file button", onClickHandler = null }: FileButtonTypes) {
+function FileButton({
+    file,
+    label = "file button",
+    onClickHandler = null,
+    size = "m",
+}: FileButtonTypes) {
+    const fileNameSizes = getSizes(size, "text");
+    const fileSizeSizes = getSizes(size, "description");
+
     return (
         <div className={styles["container"]} aria-label={label}>
             <div className={styles["file-info"]}>
-                <p className={styles["file-name"]}>{file.name}</p>
-                <p className={styles["file-size"]}>{formatBytes(file.size, 2)}</p>
+                <p className={styles["file-name"]} style={{ ...fileNameSizes }}>
+                    {file.name}
+                </p>
+                <p className={styles["file-size"]} style={{ ...fileSizeSizes }}>
+                    {formatBytes(file.size, 2)}
+                </p>
             </div>
             <Buttons.Basic
                 text=""
