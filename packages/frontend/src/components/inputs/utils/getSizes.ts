4554911d2b44aaa0b18b547ff1562d755db19190
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-01 12:56:47 +0000

    Add 'text' styles to getSizes file

--- packages/frontend/src/components/inputs/utils/getSizes.ts
+++ packages/frontend/src/components/inputs/utils/getSizes.ts
@@ -1,8 +1,8 @@
 const getSizes = (
     size: "xs" | "s" | "m" | "l" | "xl",
-    element: "label" | "description" | "input" | "counter" | "error",
+    element: "label" | "description" | "input" | "counter" | "error" | "text",
 ): { [key: string]: string } => {
-    let sizes = { label: {}, description: {}, input: {}, counter: {}, error: {} };
+    let sizes = { label: {}, description: {}, input: {}, counter: {}, error: {}, text: {} };
     switch (size) {
         case "xs":
             sizes = {
@@ -20,6 +20,9 @@ const getSizes = (
                 },
                 error: {
                     fontSize: "0.6rem",
+                },
+                text: {
+                    fontSize: "0.75rem",
                 },
             };
             break;
@@ -40,6 +43,9 @@ const getSizes = (
                 error: {
                     fontSize: "0.8rem",
                 },
+                text: {
+                    fontSize: "1.0rem",
+                },
             };
             break;
         case "l":
@@ -58,6 +64,9 @@ const getSizes = (
                 },
                 error: {
                     fontSize: "1.2rem",
+                },
+                text: {
+                    fontSize: "1.5rem",
                 },
             };
             break;
@@ -78,6 +87,9 @@ const getSizes = (
                 error: {
                     fontSize: "1.4rem",
                 },
+                text: {
+                    fontSize: "1.75rem",
+                },
             };
             break;
         case "m":
@@ -97,6 +109,9 @@ const getSizes = (
                 },
                 error: {
                     fontSize: "1.0rem",
+                },
+                text: {
+                    fontSize: "1.25rem",
                 },
             };
             break;
