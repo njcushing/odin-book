b92cd3d55a24d519cb1ecdb252e1ea7c67100768
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-01 10:57:29 +0000

    Write post text and image validator functions

--- packages/shared/src/validation/index.ts
+++ packages/shared/src/validation/index.ts
@@ -1,7 +1,9 @@
 import * as user from "./user";
+import * as post from "./post";
 
 const validation = {
     user,
+    post,
 };
 
 export default validation;
---
+++ packages/shared/src/validation/post.ts
@@ -0,0 +1,50 @@
+import * as extendedTypes from "@shared/utils/extendedTypes";
+
+type returnTypes = {
+    status: boolean;
+    message: string;
+};
+
+export const text = (value: string, messageType: "front" | "back"): returnTypes => {
+    if (value.length > 500) {
+        return {
+            status: false,
+            message:
+                messageType === "front"
+                    ? "Post text must be at most 500 characters in length."
+                    : "'text' field (String) must be at most 500 characters in length",
+        };
+    }
+    return {
+        status: true,
+        message: messageType === "front" ? "Valid post text." : "'text' field (String) is valid",
+    };
+};
+
+export const image = (
+    value: extendedTypes.TypedArray,
+    messageType: "front" | "back",
+): returnTypes => {
+    if (!extendedTypes.isTypedArray(value)) {
+        return {
+            status: false,
+            message:
+                messageType === "front"
+                    ? `Post image must be a Typed Array.`
+                    : `'image' field must be a Typed Array.`,
+        };
+    }
+    if (value.length > 2000000) {
+        return {
+            status: false,
+            message:
+                messageType === "front"
+                    ? `Post image must be smaller than 2MB.`
+                    : `'image' field (Array) must be smaller than 2MB.`,
+        };
+    }
+    return {
+        status: true,
+        message: messageType === "front" ? `Valid post image.` : `'image' field (Array) is valid`,
+    };
+};
