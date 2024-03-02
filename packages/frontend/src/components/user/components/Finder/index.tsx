d3df582c370737addabb364dc881d483a4ca1506
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-02 13:59:19 +0000

    Write functionality and style rules for user Finder component

--- packages/frontend/src/components/user/components/Finder/index.tsx
+++ packages/frontend/src/components/user/components/Finder/index.tsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import Inputs from "@/components/inputs";
+import Buttons from "@/components/buttons";
+import * as ButtonTypes from "@/components/buttons/types";
+import * as Types from "../../types";
+import User from "../..";
 import styles from "./index.module.css";
 
-function Finder() {
-    return <div className={styles["container"]}></div>;
+type FinderTypes = {
+    placeholder?: string;
+    button?: ButtonTypes.Basic;
+};
+
+function Finder({ placeholder, button }: FinderTypes) {
+    const [user, setUser] = useState<Types.ImageAndName | null>(null);
+
+    return (
+        <div className={styles["container"]}>
+            <Inputs.Search
+                onSearchHandler={() => {
+                    /* find user */
+                }}
+                placeholder={placeholder}
+            />
+            {user && (
+                <div className={styles["found-user"]}>
+                    <User.ImageAndName
+                        image={{ src: user.image.src, alt: user.image.alt }}
+                        displayName={user.displayName}
+                        accountTag={user.accountTag}
+                        size="m"
+                    />
+                    {button && <Buttons.Basic {...button} />}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Finder;
