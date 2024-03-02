e4fe9b5fa24b3c90fc9f230c9a3fa30d60f2f81f
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-02 12:22:55 +0000

    Write boilerplate for user Finder component

---
+++ packages/frontend/src/components/user/components/Finder/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./index.module.css";
+
+function Finder() {
+    return <div className={styles["container"]}></div>;
+}
+
+export default Finder;
--- packages/frontend/src/components/user/index.ts
+++ packages/frontend/src/components/user/index.ts
@@ -1,11 +1,13 @@
 import ImageAndName from "./components/ImageAndName";
 import Option from "./components/Option";
 import Summary from "./components/Summary";
+import Finder from "./components/Finder";
 
 const User = {
     ImageAndName,
     Option,
     Summary,
+    Finder,
 };
 
 export default User;
