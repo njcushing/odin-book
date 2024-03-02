3f9ec6539eb1cb429e4598a27bbc2c3b80b297d8
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-02 14:00:29 +0000

    Write boilerplate for user Selector component

--- packages/frontend/src/components/user/index.ts
+++ packages/frontend/src/components/user/index.ts
@@ -2,12 +2,14 @@ import ImageAndName from "./components/ImageAndName";
 import Option from "./components/Option";
 import Summary from "./components/Summary";
 import Finder from "./components/Finder";
+import Selector from "./components/Selector";
 
 const User = {
     ImageAndName,
     Option,
     Summary,
     Finder,
+    Selector,
 };
 
 export default User;
