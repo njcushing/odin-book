5bd408e9da4605bb8d2f4024fe9876af1a185f5d
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-03 13:54:10 +0000

    Write basic functionality and style rules for 'Main' settings component
    The Settings page will now display a list of settings sections, each of which contains the individual forms and inputs to update that section's settings.

--- packages/frontend/src/features/settings/Main/index.tsx
+++ packages/frontend/src/features/settings/Main/index.tsx
@@ -1,7 +1,88 @@
+import { Outlet } from "react-router-dom";
+import LayoutUI from "@/layouts";
+import Navigation from "@/components/navigation";
+import Sections from "../utils/Sections";
 import styles from "./index.module.css";
 
+export const routes = [
+    {
+        path: "",
+        element: <div></div>,
+        errorElement: <div></div>,
+    },
+    {
+        path: "account",
+        element: <Sections.Account />,
+        errorElement: <div></div>,
+    },
+    {
+        path: "preferences",
+        element: <Sections.Preferences />,
+        errorElement: <div></div>,
+    },
+    {
+        path: "profile",
+        element: <Sections.Profile />,
+        errorElement: <div></div>,
+    },
+];
+
 function Main() {
-    return <div className={styles["container"]}></div>;
+    const optionStyles = {
+        gap: "0.4rem",
+        fontSize: "1.2rem",
+        padding: "0.3rem 0.4rem",
+    };
+
+    const navigationOptions = [
+        { text: "Account", symbol: "lock", link: "account", style: optionStyles },
+        { text: "Preferences", symbol: "palette", link: "preferences", style: optionStyles },
+        { text: "Profile", symbol: "person", link: "profile", style: optionStyles },
+    ];
+
+    const navigation = (
+        <div className={styles["navigation-wrapper"]} key={0}>
+            <div className={styles["navigation-container"]}>
+                <Navigation.Menu type="wide" options={navigationOptions} />
+            </div>
+        </div>
+    );
+
+    return (
+        <div className={styles["container"]}>
+            <LayoutUI.Spatial
+                width="100%"
+                height="100%"
+                arrangements={[
+                    {
+                        type: "columns",
+                        minWidth: 0,
+                        maxWidth: 999999,
+                        minHeight: 0,
+                        maxHeight: 999999,
+                        areas: [
+                            { size: "40%", children: [navigation] },
+                            {
+                                size: "60%",
+                                children: [
+                                    <div className={styles["right-panel"]} key={0}>
+                                        <Outlet />
+                                    </div>,
+                                ],
+                            },
+                        ],
+                        style: {
+                            justifySelf: "flex-start",
+                            alignSelf: "center",
+                            width: "100%",
+                            height: "100%",
+                            padding: "0rem",
+                        },
+                    },
+                ]}
+            />
+        </div>
+    );
 }
 
 export default Main;
--- packages/frontend/src/features/settings/index.ts
+++ packages/frontend/src/features/settings/index.ts
@@ -1,7 +1,11 @@
-import Main from "./Main";
+import Main, { routes as MainRoutes } from "./Main";
 
 const Settings = {
     Main,
 };
 
 export default Settings;
+
+export const Routes = {
+    Main: MainRoutes,
+};
