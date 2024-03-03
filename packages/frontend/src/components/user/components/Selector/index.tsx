5ab1501110dc87521890863af0dfbd3969bf1eb2
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-03 11:45:57 +0000

    Write functionality for user Selector component

--- packages/frontend/src/components/user/components/Selector/index.tsx
+++ packages/frontend/src/components/user/components/Selector/index.tsx
@@ -1,7 +1,77 @@
+import { useState } from "react";
+import Buttons from "@/components/buttons";
+import * as ButtonTypes from "@/components/buttons/types";
+import * as ModelTypes from "@/utils/modelTypes";
+import User from "../..";
 import styles from "./index.module.css";
 
+type UserTypesPicked = Pick<ModelTypes.User, "_id" | "accountTag"> & {
+    preferences: Pick<ModelTypes.User["preferences"], "displayName">;
+};
+
+type Users = {
+    [key: string]: UserTypesPicked;
+};
+
 function Selector() {
-    return <div className={styles["container"]}></div>;
+    const [selectedUsers, setSelectedUsers] = useState<Users>({});
+
+    const addButton: ButtonTypes.Basic = {
+        text: "",
+        symbol: "add",
+        palette: "green",
+        otherStyles: { fontSize: "1.2rem", padding: "0.2rem" },
+    };
+
+    return (
+        <div className={styles["container"]}>
+            <User.Finder
+                placeholder="Search for users..."
+                button={addButton}
+                onClickHandler={(user) => {
+                    const newSelectedUsers: Users = {};
+                    const key = user._id;
+                    newSelectedUsers[key] = {
+                        _id: user._id,
+                        accountTag: user.accountTag,
+                        preferences: {
+                            displayName: user.preferences.displayName,
+                        },
+                    };
+                    setSelectedUsers({ ...selectedUsers, ...newSelectedUsers });
+                }}
+                clearFindOnClick
+            />
+            {Object.keys(selectedUsers).length > 0 && (
+                <ul className={styles["selected-users"]}>
+                    {Object.keys(selectedUsers).map((key) => {
+                        const user = selectedUsers[key];
+                        return (
+                            <li className={styles["user"]} key={user._id}>
+                                <div className={styles["user-info"]}>
+                                    <p className={styles["display-name"]}>
+                                        {user.preferences.displayName}
+                                    </p>
+                                    <p className={styles["account-tag"]}>@{user.accountTag}</p>
+                                </div>
+                                <Buttons.Basic
+                                    text=""
+                                    symbol="remove"
+                                    onClickHandler={() => {
+                                        const newSelectedUsers = { ...selectedUsers };
+                                        delete newSelectedUsers[key];
+                                        setSelectedUsers(newSelectedUsers);
+                                    }}
+                                    palette="red"
+                                    otherStyles={{ fontSize: "1.0rem", padding: "0.2rem" }}
+                                />
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
+        </div>
+    );
 }
 
 export default Selector;
