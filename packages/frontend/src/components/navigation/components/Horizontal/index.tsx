38a677f91f270d46f6e32ce56c95650f3dafbda2
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-05 22:01:53 +0000

    Remove menu from Horizontal navigation component
    I have instead decided to display all available menu options; they now reside in a flex container that wraps the options when needed.

--- packages/frontend/src/components/navigation/components/Horizontal/index.tsx
+++ packages/frontend/src/components/navigation/components/Horizontal/index.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
-import OutsideClickHandler from "react-outside-click-handler";
 import styles from "./index.module.css";
 
 type Option = {
@@ -27,111 +26,14 @@ function Horizontal({
     style = {},
 }: HorizontalTypes) {
     const [optionSelected, setOptionSelected] = useState<string | null>(selected);
-    const [optionsDisplaying, setOptionsDisplaying] = useState<Option[]>(options);
-    const [optionsInMenu, setOptionsInMenu] = useState<Option[]>([]);
-    const [displayMenuButton, setDisplayMenuButton] = useState<boolean>(false);
-    const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
     const listRef = useRef<HTMLUListElement>(null);
-    const hiddenMenuButtonContainerRef = useRef<HTMLDivElement>(null);
-    const menuButtonContainerRef = useRef<HTMLDivElement>(null);
-
     const fontSize = style.fontSize || "1.1rem";
-
-    const selectedOptionVisible =
-        optionSelected && optionsInMenu.map((option) => option.text).indexOf(optionSelected) === -1;
-
-    const menuButton = (
-        <button
-            type="button"
-            className={styles["option"]}
-            aria-label="more options"
-            data-selected={menuOpen}
-            data-highlighted={!selectedOptionVisible}
-            onClick={() => setMenuOpen(!menuOpen)}
-        >
-            <p
-                className={`material-symbols-rounded ${styles["symbol"]}`}
-                style={{
-                    fontSize,
-                    paddingLeft: minPaddingPx,
-                    paddingRight: minPaddingPx,
-                }}
-            >
-                menu
-            </p>
-            <div className={styles["highlight-bar"]}></div>
-        </button>
-    );
-
-    useEffect(() => {
-        let listRefCurrent: Element;
-        const observer = new ResizeObserver((entries) => {
-            // calculate maximum number of options that can appear in available space
-            const textWidths: number[] = [];
-            Array.from(entries[0].target.children).forEach((option) => {
-                const optionText = option.children[0];
-                textWidths.push(optionText.clientWidth);
-            });
-            const optionsToDisplay: Option[] = [];
-            const maximumWidth = entries[0].contentRect.width;
-            let currentWidthTotal = 0;
-            for (let i = 0; i < textWidths.length; i++) {
-                const optionWidth = textWidths[i] + 2 * minPaddingPx;
-                if (currentWidthTotal + optionWidth > maximumWidth) {
-                    break;
-                } else {
-                    optionsToDisplay.push(options[i]);
-                    currentWidthTotal += optionWidth;
-                }
-            }
-
-            /*
-             *  if there are too many options to fit in the available space, see if the menu button
-             *  can fit in the remaining space. If not, remove the last option to make room.
-             */
-            const menuButtonWidth = hiddenMenuButtonContainerRef.current
-                ? hiddenMenuButtonContainerRef.current.clientWidth
-                : 0;
-            if (optionsToDisplay.length < options.length) {
-                if (currentWidthTotal + menuButtonWidth > maximumWidth) {
-                    optionsToDisplay.pop();
-                }
-                setDisplayMenuButton(true);
-            } else {
-                setDisplayMenuButton(false);
-            }
-            setOptionsDisplaying(optionsToDisplay);
-
-            const optionsToHide: Option[] = [];
-            for (let i = optionsToDisplay.length; i < options.length; i++) {
-                optionsToHide.push(options[i]);
-            }
-            setOptionsInMenu(optionsToHide);
-        });
-        if (listRef.current) {
-            listRefCurrent = listRef.current;
-            observer.observe(listRef.current);
-        }
-        return () => {
-            if (listRefCurrent instanceof Element) observer.unobserve(listRefCurrent);
-        };
-    }, [options, minPaddingPx]);
-
-    let gridColumnWidths = "";
-    optionsDisplaying.forEach(() => {
-        gridColumnWidths += "auto ";
-    });
-    if (displayMenuButton) gridColumnWidths += "min-content";
 
     return (
         <nav className={styles["container"]} aria-label={label} style={{ ...style }}>
-            <ul
-                className={styles["list"]}
-                style={{ gridTemplateColumns: gridColumnWidths }}
-                ref={listRef}
-            >
-                {optionsDisplaying.map((option) => {
+            <ul className={styles["list"]} ref={listRef}>
+                {options.map((option) => {
                     return (
                         <Link
                             to={option.link || ""}
@@ -161,65 +63,7 @@ function Horizontal({
                         </Link>
                     );
                 })}
-                {displayMenuButton ? <div ref={menuButtonContainerRef}>{menuButton}</div> : null}
             </ul>
-            {menuOpen ? (
-                <OutsideClickHandler onOutsideClick={() => setMenuOpen(false)}>
-                    <ul
-                        className={styles["menu"]}
-                        style={
-                            menuButtonContainerRef.current
-                                ? {
-                                      top:
-                                          menuButtonContainerRef.current.offsetTop +
-                                          menuButtonContainerRef.current.offsetHeight +
-                                          4,
-                                      right: 4,
-                                  }
-                                : { top: 4, right: 4 }
-                        }
-                    >
-                        {optionsInMenu.map((option) => {
-                            return (
-                                <Link
-                                    to={option.link || ""}
-                                    className={styles["option"]}
-                                    data-highlighted={!!(optionSelected === option.text)}
-                                    onClick={(e: React.MouseEvent<HTMLAnchorElement>) => {
-                                        setOptionSelected(option.text);
-                                        if (onSelectHandler) onSelectHandler(e);
-                                        setMenuOpen(false);
-                                        e.currentTarget.blur();
-                                    }}
-                                    onMouseLeave={(e) => {
-                                        e.currentTarget.blur();
-                                    }}
-                                    key={option.text}
-                                >
-                                    <div
-                                        className={styles["text"]}
-                                        style={{
-                                            fontSize,
-                                            paddingLeft: minPaddingPx,
-                                            paddingRight: minPaddingPx,
-                                        }}
-                                    >
-                                        {option.text}
-                                    </div>
-                                    <div className={styles["highlight-bar"]}></div>
-                                </Link>
-                            );
-                        })}
-                    </ul>
-                </OutsideClickHandler>
-            ) : null}
-            <div
-                className={styles["menu-button-false-render"]}
-                hidden
-                ref={hiddenMenuButtonContainerRef}
-            >
-                {menuButton}
-            </div>
         </nav>
     );
 }
