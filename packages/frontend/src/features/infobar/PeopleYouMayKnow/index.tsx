9ddfbd79fe88c809330372799ac8c29d8363a751
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-14 00:22:05 +0000

    Utilise useAsync.GET hook in PeopleYouMayKnow component

--- packages/frontend/src/features/infobar/PeopleYouMayKnow/index.tsx
+++ packages/frontend/src/features/infobar/PeopleYouMayKnow/index.tsx
@@ -1,36 +1,40 @@
 import User from "@/components/user";
 import Buttons from "@/components/buttons";
-import * as mockData from "@/mockData";
+import * as modelTypes from "@/utils/modelTypes";
+import * as useAsync from "@/hooks/useAsync";
+import getRelatedUsers from "./utils/getRelatedUsers";
 import styles from "./index.module.css";
 
 function PeopleYouMayKnow() {
-    const users = mockData.getUsers(3);
+    const [users] = useAsync.GET<modelTypes.User[]>([], { func: getRelatedUsers }, true);
 
     return (
         <div className={styles["container"]}>
             <h3 className={styles["title"]}>People You May Know</h3>
             <ul className={styles["users"]}>
-                {users.map((user) => {
-                    return (
-                        <li className={styles["user"]} key={user._id}>
-                            <User.Option
-                                user={{
-                                    image: {
-                                        src: user.preferences.profileImage.src,
-                                        alt: user.preferences.profileImage.alt,
-                                        status: user.status,
-                                    },
-                                    displayName: user.preferences.displayName,
-                                    accountTag: user.accountTag,
-                                    size: "s",
-                                }}
-                                following
-                            />
-                        </li>
-                    );
-                })}
+                {users &&
+                    users.map((user) => {
+                        return (
+                            <li className={styles["user"]} key={user._id}>
+                                <User.Option
+                                    user={{
+                                        image: {
+                                            src: user.preferences.profileImage.src,
+                                            alt: user.preferences.profileImage.alt,
+                                            status: user.status,
+                                        },
+                                        displayName: user.preferences.displayName,
+                                        accountTag: user.accountTag,
+                                        size: "s",
+                                    }}
+                                    onClickHandler={() => {}}
+                                    following
+                                />
+                            </li>
+                        );
+                    })}
             </ul>
-            <Buttons.Basic text="See More" />
+            <Buttons.Basic text="See More" onClickHandler={() => {}} />
         </div>
     );
 }
