b0fdcd0e3f561e0f53caa8fa19cc601c10e41e17
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-14 09:28:26 +0000

    Update layout for CreateAccount and Login page components

--- packages/frontend/src/pages/CreateAccount/index.tsx
+++ packages/frontend/src/pages/CreateAccount/index.tsx
@@ -6,70 +6,72 @@ import styles from "./index.module.css";
 
 function CreateAccount() {
     return (
-        <div className={styles["container"]}>
-            <Forms.Basic
-                title="Create a New Account"
-                sections={[
-                    {
-                        fields: [
-                            <Inputs.Text
-                                labelText="Username"
-                                fieldId="username"
-                                fieldName="username"
-                                validator={{ func: validation.user.username }}
-                                required
-                                key={0}
-                            />,
-                            <Inputs.Text
-                                type="email"
-                                labelText="Email"
-                                fieldId="email"
-                                fieldName="email"
-                                validator={{ func: validation.user.email }}
-                                required
-                                key={1}
-                            />,
-                            <Inputs.Text
-                                type="password"
-                                labelText="Password"
-                                fieldId="password"
-                                fieldName="password"
-                                validator={{ func: validation.user.password }}
-                                required
-                                key={1}
-                            />,
-                            <Inputs.Text
-                                type="password"
-                                labelText="Confirm Password"
-                                fieldId="confirmPassword"
-                                fieldName="confirmPassword"
-                                validator={{ func: validation.user.confirmPassword }}
-                                required
-                                key={1}
-                            />,
-                        ],
-                    },
-                ]}
-                onSubmitHandler={(e: React.MouseEvent<HTMLButtonElement>) => {
-                    const target = e.target as HTMLButtonElement;
-                    if (target.form) {
-                        const formData = new FormData(target.form);
-                        const formFields = Object.fromEntries(formData);
-                    }
-                }}
-                button={{
-                    text: "Create Account",
-                    palette: "green",
-                }}
-                key={0}
-            />
-            <Buttons.Basic
-                text="Return to Login"
-                onClickHandler={() => {
-                    window.location.href = "/login";
-                }}
-                palette="bare"
-            />
+        <div className={styles["wrapper"]}>
+            <div className={styles["container"]}>
+                <Forms.Basic
+                    title="Create a New Account"
+                    sections={[
+                        {
+                            fields: [
+                                <Inputs.Text
+                                    labelText="Username"
+                                    fieldId="username"
+                                    fieldName="username"
+                                    validator={{ func: validation.user.username }}
+                                    required
+                                    key={0}
+                                />,
+                                <Inputs.Text
+                                    type="email"
+                                    labelText="Email"
+                                    fieldId="email"
+                                    fieldName="email"
+                                    validator={{ func: validation.user.email }}
+                                    required
+                                    key={1}
+                                />,
+                                <Inputs.Text
+                                    type="password"
+                                    labelText="Password"
+                                    fieldId="password"
+                                    fieldName="password"
+                                    validator={{ func: validation.user.password }}
+                                    required
+                                    key={1}
+                                />,
+                                <Inputs.Text
+                                    type="password"
+                                    labelText="Confirm Password"
+                                    fieldId="confirmPassword"
+                                    fieldName="confirmPassword"
+                                    validator={{ func: validation.user.confirmPassword }}
+                                    required
+                                    key={1}
+                                />,
+                            ],
+                        },
+                    ]}
+                    onSubmitHandler={(e: React.MouseEvent<HTMLButtonElement>) => {
+                        const target = e.target as HTMLButtonElement;
+                        if (target.form) {
+                            const formData = new FormData(target.form);
+                            const formFields = Object.fromEntries(formData);
+                        }
+                    }}
+                    button={{
+                        text: "Create Account",
+                        palette: "green",
+                    }}
+                    key={0}
+                />
+                <Buttons.Basic
+                    text="Return to Login"
+                    onClickHandler={() => {
+                        window.location.href = "/login";
+                    }}
+                    palette="bare"
+                />
+            </div>
         </div>
     );
 }
--- packages/frontend/src/pages/Login/index.tsx
+++ packages/frontend/src/pages/Login/index.tsx
@@ -5,53 +5,55 @@ import styles from "./index.module.css";
 
 function Login() {
     return (
-        <div className={styles["container"]}>
-            <Forms.Basic
-                title="Log In"
-                sections={[
-                    {
-                        fields: [
-                            <Inputs.Text
-                                labelText="Username"
-                                fieldId="username"
-                                fieldName="username"
-                                key={0}
-                            />,
-                            <Inputs.Text
-                                type="password"
-                                labelText="Password"
-                                fieldId="password"
-                                fieldName="password"
-                                key={1}
-                            />,
-                        ],
-                    },
-                ]}
-                onSubmitHandler={(e: React.MouseEvent<HTMLButtonElement>) => {
-                    const target = e.target as HTMLButtonElement;
-                    if (target.form) {
-                        const formData = new FormData(target.form);
-                        const formFields = Object.fromEntries(formData);
-                    }
-                }}
-                button={{
-                    text: "Log In",
-                    palette: "green",
-                }}
-                key={0}
-            />
-            <p className={styles["create-account-message"]}>
-                {`Don't have an account? `}
-                <strong>Create one now</strong>
-                {` by clicking the button below.`}
-            </p>
-            <Buttons.Basic
-                text="Create Account"
-                onClickHandler={() => {
-                    window.location.href = "/create-account";
-                }}
-                palette="gold"
-            />
+        <div className={styles["wrapper"]}>
+            <div className={styles["container"]}>
+                <Forms.Basic
+                    title="Log In"
+                    sections={[
+                        {
+                            fields: [
+                                <Inputs.Text
+                                    labelText="Username"
+                                    fieldId="username"
+                                    fieldName="username"
+                                    key={0}
+                                />,
+                                <Inputs.Text
+                                    type="password"
+                                    labelText="Password"
+                                    fieldId="password"
+                                    fieldName="password"
+                                    key={1}
+                                />,
+                            ],
+                        },
+                    ]}
+                    onSubmitHandler={(e: React.MouseEvent<HTMLButtonElement>) => {
+                        const target = e.target as HTMLButtonElement;
+                        if (target.form) {
+                            const formData = new FormData(target.form);
+                            const formFields = Object.fromEntries(formData);
+                        }
+                    }}
+                    button={{
+                        text: "Log In",
+                        palette: "green",
+                    }}
+                    key={0}
+                />
+                <p className={styles["create-account-message"]}>
+                    {`Don't have an account? `}
+                    <strong>Create one now</strong>
+                    {` by clicking the button below.`}
+                </p>
+                <Buttons.Basic
+                    text="Create Account"
+                    onClickHandler={() => {
+                        window.location.href = "/create-account";
+                    }}
+                    palette="gold"
+                />
+            </div>
         </div>
     );
 }
