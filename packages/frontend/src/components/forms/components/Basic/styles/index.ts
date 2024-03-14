dc83afcf423f1b4fa6b3a6b592444836457b29e0
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-14 11:47:11 +0000

    Add function to Basic form component's styles module
    The function takes one argument: a string, and returns a CSS module.

--- packages/frontend/src/components/forms/components/Basic/styles/index.ts
+++ packages/frontend/src/components/forms/components/Basic/styles/index.ts
@@ -1,9 +1,21 @@
 import plain from "./plain.module.css";
 import style1 from "./style1.module.css";
 
+type Style = "plain" | "1";
+
 const styles = {
     plain,
     style1,
+};
+
+export const getStyles = (style: Style) => {
+    switch (style) {
+        case "1":
+            return styles.style1;
+        case "plain":
+        default:
+            return styles.plain;
+    }
 };
 
 export default styles;
