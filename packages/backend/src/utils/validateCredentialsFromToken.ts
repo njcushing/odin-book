744b6b68a601ccb2046d8260ba99fb3f7ca3fd7d
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-15 11:34:43 +0000

    Write function that validates user from extracted JSONWebToken payload

---
+++ packages/backend/src/utils/validateCredentialsFromToken.ts
@@ -0,0 +1,28 @@
+import bcrypt from "bcryptjs";
+import User from "@/models/user.js";
+import * as Types from "./types";
+
+const validateCredentialsFromToken = async (payload: Types.Token) => {
+    const { accountTag, password } = payload;
+    const { githubId } = payload.providerIds || { githubId: null };
+
+    let user;
+    if (githubId) {
+        // for a github login, only the githubId needs to be verified
+        user = await User.findOne({ githubId }, { _id: 1, githubId: 1 }).exec();
+        if (!user) return [false, null, "Incorrect githubId."];
+    } else if (accountTag && password) {
+        // for an account tag + password login, both need to be verified
+        user = await User.findOne({ accountTag }, { _id: 1, accountTag: 1, password: 1 }).exec();
+        if (!user) return [false, null, "Incorrect account tag."];
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) return [false, null, "Incorrect password."];
+        user.password = password; // Ensure password returned is not the hashed version
+    } else {
+        return [false, null, "No credentials found."];
+    }
+
+    return [true, user, null];
+};
+
+export default validateCredentialsFromToken;
