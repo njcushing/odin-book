a3a20467c4abefdeabeb79f8a18791fa84bb49b5
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-19 21:10:08 +0000

    Write functionality for /auth/github/callback route

--- packages/backend/src/controllers/auth/github/index.tsx
+++ packages/backend/src/controllers/auth/github/index.tsx
@@ -1,5 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import asyncHandler from "express-async-handler";
+import sendResponse from "@/utils/sendResponse";
+import generateToken from "@/utils/generateToken";
+import * as Types from "@/utils/types";
 import passport from "passport";
 
 export const get = [passport.authenticate("github", { session: false, scope: ["user:email"] })];
 
-export const callback = [passport.authenticate("github", { session: false })];
+export const callback = [
+    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+        passport.authenticate(
+            "github",
+            { session: false },
+            async (
+                err: Types.ResponseError | null,
+                user: { providers: { provider: string; providerId: string }[] } | null,
+                info?: { message?: string },
+            ) => {
+                if (err) {
+                    return sendResponse(
+                        res,
+                        err.status,
+                        (info && info.message) || err.message,
+                        null,
+                        err,
+                    );
+                }
+                if (user) {
+                    const provider = user.providers.find((prov) => prov.providerId === "github");
+                    const token = await generateToken({ providedBy: provider });
+                    return sendResponse(res, 200, "Successful login with GitHub", { token });
+                }
+                return sendResponse(res, 500, "Something went wrong", null);
+            },
+        )(req, res, next);
+    }),
+];
