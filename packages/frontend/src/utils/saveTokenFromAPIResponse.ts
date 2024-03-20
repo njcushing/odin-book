ed8437a074b54355f92b6616cc7587f43fc458fc
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-20 21:30:32 +0000

    Write function that saves token from API response

---
+++ packages/frontend/src/utils/saveTokenFromAPIResponse.ts
@@ -0,0 +1,12 @@
+const saveTokenFromAPIResponse = async (response: { data?: { token?: string } }) => {
+    if (
+        response.data !== null &&
+        typeof response.data === "object" &&
+        "token" in response.data &&
+        typeof response.data.token === "string"
+    ) {
+        localStorage.setItem("odin-book--auth-token", response.data.token);
+    }
+};
+
+export default saveTokenFromAPIResponse;
