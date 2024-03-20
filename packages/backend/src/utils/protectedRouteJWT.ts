7d8b27ca9d2a86505e5dcdbdcded66f22f55e035
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-20 20:44:47 +0000

    Create middleware that handles authentication logic for passport JWT strategy

---
+++ packages/backend/src/utils/protectedRouteJWT.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import passport from "passport";
+import * as Types from "@/utils/types";
+import sendResponse from "./sendResponse.js";
+
+const protectedRouteJWT = (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate(
+        "jwt",
+        { session: false },
+        (err: Types.ResponseError, user?: object | null, info?: { message?: string }) => {
+            if (err) {
+                return next(
+                    sendResponse(res, err.status, (info && info.message) || err.message, null, err),
+                );
+            }
+            if (user) {
+                req.user = user;
+                return next();
+            }
+            return next(sendResponse(res, 500, (info && info.message) || "Something went wrong"));
+        },
+    )(req, res, next);
+};
+
+export default protectedRouteJWT;
