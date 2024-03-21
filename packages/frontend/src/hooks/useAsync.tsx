30da292433e88fb30d5c8e0af5a767d292567f35
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-21 09:46:02 +0000

    Create custom hook to use async API POST functions in components

--- packages/frontend/src/hooks/useAsync.tsx
+++ packages/frontend/src/hooks/useAsync.tsx
@@ -53,3 +53,57 @@ export function GET<T>(
 
     return [value, errorMessage, setAttempting];
 }
+
+export function POST<T>(
+    initialValue: T,
+    functionInfo: {
+        func: apiFunctionTypes.GET<T>;
+        data?: {
+            params?: { [key: string | number]: unknown };
+            body?: object;
+        };
+        args?: unknown[];
+    },
+    attemptOnMount?: boolean,
+): [T | null, string, React.Dispatch<React.SetStateAction<boolean>>] {
+    const [value, setValue] = useState<T | null>(initialValue);
+    const [abortController, setAbortController] = useState<AbortController | null>(
+        new AbortController(),
+    );
+    const [attempting, setAttempting] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
+    useEffect(() => {
+        if (attemptOnMount) setAttempting(true);
+    }, [attemptOnMount]);
+
+    useEffect(() => {
+        if (attempting) {
+            if (abortController) abortController.abort();
+            const abortControllerNew = new AbortController();
+            setAbortController(abortControllerNew);
+            (async () => {
+                const { status, message, data } = await functionInfo.func(
+                    functionInfo.data,
+                    abortController,
+                    functionInfo.args,
+                );
+                if (status) {
+                    setValue(data || null);
+                    setErrorMessage("");
+                } else {
+                    setValue(null);
+                    setErrorMessage(message || "");
+                }
+                setAbortController(null);
+                setAttempting(false);
+            })();
+        }
+
+        return () => {
+            if (abortController) abortController.abort();
+        };
+    }, [abortController, attempting, functionInfo]);
+
+    return [value, errorMessage, setAttempting];
+}
