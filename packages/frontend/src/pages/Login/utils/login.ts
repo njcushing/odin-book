b76312d2fe156e99d4be3f823bc19c47e2745435
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-21 13:01:06 +0000

    Write api function for logging in the user with standard credentials

---
+++ packages/frontend/src/pages/Login/utils/login.ts
@@ -0,0 +1,35 @@
+import * as apiFunctionTypes from "@shared/utils/apiFunctionTypes";
+import saveTokenFromAPIResponse from "@/utils/saveTokenFromAPIResponse";
+
+const login: apiFunctionTypes.POST = async (data, abortController = null) => {
+    let body;
+    if (data && data.body) body = data.body;
+
+    const result = await fetch(`${import.meta.env.VITE_SERVER_DOMAIN}/auth/login`, {
+        signal: abortController ? abortController.signal : null,
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    })
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+            };
+        });
+    return result;
+};
+
+export default login;
