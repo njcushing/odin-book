d1ba7269fb7f8cfd8aa43ec94f04602f8e04669e
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-24 12:37:40 +0000

    Write functionality for /user/active GET route
    This route contains an aggregation that will return various fields for the logged-in user.

--- packages/backend/src/controllers/user/index.ts
+++ packages/backend/src/controllers/user/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import * as information from "./information";
 import * as createAccount from "./create-account";
 
 const router = express.Router();
+
+router.get("/active", information.active);
 
 router.post("/create-account", createAccount.post);
 
