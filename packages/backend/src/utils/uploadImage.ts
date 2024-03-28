fcf5728c6248e8d9b85426cf25292a57541ea0d8
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-28 11:43:05 +0000

    Add new 'multiple' function for uploading multiple images at once
    This function also has the option to destroy all the successfully-uploaded images if at least one of them fails to upload.

--- packages/backend/src/utils/uploadImage.ts
+++ packages/backend/src/utils/uploadImage.ts
@@ -1,4 +1,4 @@
-import { v2 as cloudinary, UploadApiOptions } from "cloudinary";
+import { v2 as cloudinary, UploadApiOptions, UploadApiResponse } from "cloudinary";
 
 const options: UploadApiOptions = {
     overwrite: true,
@@ -6,9 +6,27 @@ const options: UploadApiOptions = {
     resource_type: "auto",
 };
 
-const uploadImage = async (image: string, opts: UploadApiOptions = options) => {
+const destroy = async (image: string, opts: UploadApiOptions): Promise<UploadApiResponse> => {
     return new Promise((resolve, reject) => {
-        cloudinary.uploader.upload(image, opts, (error, result) => {
+        cloudinary.uploader.destroy(
+            image,
+            { resource_type: opts.resource_type },
+            (error, result) => {
+                if (result && result.secure_url) {
+                    return resolve(result.secure_url);
+                }
+                return reject(error);
+            },
+        );
+    });
+};
+
+export const single = async (
+    image: string,
+    opts: UploadApiOptions = options,
+): Promise<string | Error> => {
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload(`data:image/jpeg;base64,${image}`, opts, (error, result) => {
             if (result && result.secure_url) {
                 return resolve(result.secure_url);
             }
@@ -17,4 +35,34 @@ const uploadImage = async (image: string, opts: UploadApiOptions = options) => {
     });
 };
 
-export default uploadImage;
+export const multiple = async (
+    images: string[],
+    opts: UploadApiOptions & { deleteAllOnFail: boolean } = { ...options, deleteAllOnFail: true },
+): Promise<[boolean, (string | Error)[]]> => {
+    // attempt to upload all images
+    const uploads = images.map((image) => single(image, opts));
+    const uploadResponses = await Promise.all(
+        uploads.map(async (promise) => {
+            return promise.then((result) => result).catch((error) => error);
+        }),
+    );
+    // delete any successful uploads if any failed and 'deleteAllOnFail' option is true
+    let failed = false;
+    if (opts.deleteAllOnFail) {
+        const imagesToDestroy = [];
+        for (let i = 0; i < uploadResponses.length; i++) {
+            const response = uploadResponses[i];
+            if (typeof response !== "string") {
+                failed = true;
+            } else {
+                imagesToDestroy.push(response);
+            }
+        }
+    }
+    if (failed) {
+        const deletions = images.map((image) => destroy(image, opts));
+        await Promise.all(deletions);
+        return [false, uploadResponses];
+    }
+    return [!failed, uploadResponses];
+};
