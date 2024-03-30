09d4dad7bf5abc401ff1517dc9b726523e48fc18
njcushing <129661299+njcushing@users.noreply.github.com>
2024-03-30 12:18:47 +0000

    Write function to request user _id from API using their account tag

---
+++ packages/frontend/src/utils/getIdFromTag.ts
@@ -0,0 +1,52 @@
+import * as apiFunctionTypes from "@shared/utils/apiFunctionTypes";
+import * as extendedTypes from "@shared/utils/extendedTypes";
+import saveTokenFromAPIResponse from "@/utils/saveTokenFromAPIResponse";
+
+export type Params = {
+    accountTag: string;
+};
+
+export type Response = extendedTypes.MongoDBObjectId | null;
+
+const getIdFromTag: apiFunctionTypes.GET<Params, Response> = async (
+    data,
+    abortController = null,
+) => {
+    const { accountTag } = data.params as Params;
+    const queryObject = { accountTag };
+    const urlParams = new URLSearchParams();
+    Object.entries(queryObject).forEach(([key, value]) => {
+        if (value !== "" && value !== undefined && value !== null) {
+            urlParams.append(key, value);
+        }
+    });
+
+    const result = await fetch(`${import.meta.env.VITE_SERVER_DOMAIN}/user/id?${urlParams}`, {
+        signal: abortController ? abortController.signal : null,
+        method: "GET",
+        mode: "cors",
+        headers: {
+            Authorization: localStorage.getItem("odin-book-auth-token") || "",
+        },
+    })
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                data: responseJSON.data._id,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+                data: null,
+            };
+        });
+    return result;
+};
+
+export default getIdFromTag;
