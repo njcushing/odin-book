4a8eaf00ce984c5bb37d59449a2738bdf834cf7d
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-02 11:27:14 +0100

    Implement UserLikes component on /user/:userId/likes route

--- packages/frontend/src/features/profile/Main/index.tsx
+++ packages/frontend/src/features/profile/Main/index.tsx
@@ -3,12 +3,13 @@ import { useParams, useLocation, Outlet } from "react-router-dom";
 import Navigation from "@/components/navigation";
 import User from "@/components/user";
 import * as mockData from "@/mockData";
-import * as extendedTypes from "@shared/utils/extendedTypes";
 import * as useAsync from "@/hooks/useAsync";
+import mongoose from "mongoose";
 import Posts from "@/features/posts";
 import getIdFromTag, { Params, Response } from "@/utils/getIdFromTag";
 import Profile from "..";
 import UserPosts from "../UserPosts";
+import UserLikes from "../UserLikes";
 import styles from "./index.module.css";
 
 export const routes = [
@@ -33,9 +34,7 @@ export const routes = [
     },
     {
         path: "likes",
-        element: ["1", "2", "3", "4", "5"].map((_id) => {
-            return <Posts.Post _id={_id} key={`post-liked-${_id}`} />;
-        }),
+        element: <UserLikes />,
         errorElement: <div></div>,
     },
     {
@@ -85,7 +84,7 @@ export const routes = [
 ];
 
 interface ProfileState {
-    _id?: extendedTypes.MongoDBObjectId | null;
+    _id?: mongoose.Types.ObjectId | null | undefined;
     awaitingResponse: boolean;
 }
 
@@ -99,7 +98,7 @@ export const ProfileContext = createContext<ProfileState>(defaultState);
 function Main() {
     const { accountTag } = useParams();
 
-    const [state, setState] = useState<extendedTypes.MongoDBObjectId | null>(null);
+    const [state, setState] = useState<mongoose.Types.ObjectId | null | undefined>(null);
     const [response] = useAsync.GET<Params, Response>(
         { func: getIdFromTag, parameters: [{ params: { accountTag: accountTag || "" } }, null] },
         true,
