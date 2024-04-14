bcc62e131af021c2b8a83a94c6dbe609b6f71f59
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-14 13:59:28 +0100

    Overhaul Option user component
    The component now utilises the 'getOption' and 'followUser' API functions and displays the correct information for a given user as returned by the API.

--- packages/frontend/src/components/user/components/Option/index.tsx
+++ packages/frontend/src/components/user/components/Option/index.tsx
@@ -1,30 +1,105 @@
+import { useState, useEffect } from "react";
+import * as useAsync from "@/hooks/useAsync";
 import Buttons from "@/components/buttons";
+import mongoose from "mongoose";
 import User from "../..";
-import { ImageAndNameTypes } from "../ImageAndName";
 import styles from "./index.module.css";
+import getOption, {
+    Params as GetOptionParams,
+    Response as GetOptionResponse,
+} from "./utils/getOption";
+import followUser, { Params as FollowUserParams } from "./utils/followUser";
 
 export type OptionTypes = {
-    user: ImageAndNameTypes;
-    following: boolean;
-    onClickHandler?: ((event: React.MouseEvent<HTMLButtonElement>) => void) | null;
+    _id: mongoose.Types.ObjectId | undefined | null;
+    overrideOptionData?: GetOptionResponse;
 };
 
-function Option({ user, following, onClickHandler = null }: OptionTypes) {
+function Option({ _id, overrideOptionData }: OptionTypes) {
+    const [optionData, setOptionData] = useState<GetOptionResponse>(null);
+
+    // get option api handling
+    const [getOptionResponse, setGetOptionParams, getOptionAgain] = useAsync.GET<
+        GetOptionParams,
+        GetOptionResponse
+    >(
+        {
+            func: getOption,
+            parameters: [{ params: { userId: _id } }, null],
+        },
+        !overrideOptionData,
+    );
+    useEffect(() => {
+        if (!overrideOptionData) {
+            const newState = getOptionResponse ? getOptionResponse.data : null;
+            setOptionData(newState);
+        }
+    }, [overrideOptionData, getOptionResponse]);
+
+    // follow user api handling
+    const [followUserResponse, setFollowUserParams, followUserAgain] = useAsync.PUT<
+        FollowUserParams,
+        null,
+        null
+    >(
+        {
+            func: followUser,
+            parameters: [{ params: { userId: _id } }, null],
+        },
+        false,
+    );
+
+    // error message handling
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    useEffect(() => {
+        if (
+            getOptionResponse &&
+            getOptionResponse.status >= 400 &&
+            getOptionResponse.message &&
+            getOptionResponse.message.length > 0
+        ) {
+            setErrorMessage(getOptionResponse.message);
+        }
+    }, [getOptionResponse]);
+
+    // fetch option again
+    useEffect(() => {
+        if (overrideOptionData) {
+            setOptionData(overrideOptionData);
+        } else {
+            getOptionAgain(true);
+        }
+    }, [overrideOptionData, getOptionAgain, followUserResponse]);
+
     return (
-        <div className={styles["container"]}>
-            <User.ImageAndName
-                image={{ src: user.image.src, alt: user.image.alt }}
-                displayName={user.displayName}
-                accountTag={user.accountTag}
-                size="s"
-            />
-            <Buttons.Basic
-                text={following ? "Unfollow" : "Follow"}
-                symbol={following ? "person_remove" : "person_add"}
-                palette={following ? "red" : "orange"}
-                onClickHandler={onClickHandler}
-                otherStyles={{ fontSize: "1.0rem" }}
-            />
+        <div className={styles["wrapper"]}>
+            {errorMessage.length > 0 ? (
+                <p className={styles["error-message"]}>{errorMessage}</p>
+            ) : null}
+            {optionData && (
+                <div className={styles["option-container"]}>
+                    <User.ImageAndName
+                        image={
+                            optionData.preferences.profileImage
+                                ? {
+                                      src: optionData.preferences.profileImage.url,
+                                      alt: optionData.preferences.profileImage.alt,
+                                  }
+                                : { src: "", alt: "" }
+                        }
+                        displayName={optionData.preferences.displayName}
+                        accountTag={optionData.accountTag}
+                        size="s"
+                    />
+                    <Buttons.Basic
+                        text={optionData.isFollowing ? "Unfollow" : "Follow"}
+                        symbol={optionData.isFollowing ? "person_remove" : "person_add"}
+                        palette={optionData.isFollowing ? "red" : "orange"}
+                        onClickHandler={() => followUserAgain(true)}
+                        otherStyles={{ fontSize: "1.0rem" }}
+                    />
+                </div>
+            )}
         </div>
     );
 }
