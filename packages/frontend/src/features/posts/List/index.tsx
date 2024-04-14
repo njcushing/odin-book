099a4f9ed746a4d4863e9d4259140c207604d59b
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-14 11:30:24 +0100

    Update post List component
    I've added a few features into this component to make it mimic the UserPosts profile component (the error message element, 'empty' message element, it now invokes an API function and utilises a useAsync hook, etc.). As it stands, the new 'getRecommendedPosts' function is just a copy of the 'getUserPosts' function - I am still yet to write a route on the backend that fetches posts from followed users, so this function is just a placeholder for now.

--- packages/frontend/src/features/posts/List/index.tsx
+++ packages/frontend/src/features/posts/List/index.tsx
@@ -1,16 +1,75 @@
+import { useState, useEffect, useContext } from "react";
+import * as useAsync from "@/hooks/useAsync";
+import { UserContext } from "@/context/user";
 import Buttons from "@/components/buttons";
 import PubSub from "pubsub-js";
+import mongoose from "mongoose";
+import getRecommendedPosts, { Params, Response } from "./utils/getRecommendedPosts";
 import Posts from "..";
 import styles from "./index.module.css";
 
 function List() {
-    const posts = [];
+    const { user, extract } = useContext(UserContext);
+
+    const [posts, setPosts] = useState<Response>([]);
+    const [response, setParams, setAttempting] = useAsync.GET<Params, Response>(
+        {
+            func: getRecommendedPosts,
+            parameters: [
+                {
+                    params: {
+                        userId: extract("_id") as mongoose.Types.ObjectId | undefined | null,
+                        after: null,
+                        repliesOnly: false,
+                    },
+                },
+                null,
+            ],
+        },
+        true,
+    );
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
+    useEffect(() => {
+        const newState = response ? response.data : [];
+        setPosts(newState || []);
+    }, [response]);
+
+    useEffect(() => {
+        setAttempting(true);
+        setErrorMessage("");
+        setParams([
+            {
+                params: {
+                    userId: extract("_id") as mongoose.Types.ObjectId | undefined | null,
+                    after: null,
+                    repliesOnly: false,
+                },
+            },
+            null,
+        ]);
+    }, [user, extract, setParams, setAttempting]);
+
+    if (response && response.status === 401) window.location.assign("/");
+
+    useEffect(() => {
+        if (response && response.status >= 400 && response.message && response.message.length > 0) {
+            setErrorMessage(response.message);
+        }
+    }, [response]);
 
     return (
         <div className={styles["container"]}>
-            {posts.map((post) => {
-                return <Posts.Post _id={post} canToggleReplies canReply key={post} />;
-            })}
+            {errorMessage.length > 0 ? (
+                <p className={styles["error-message"]}>{errorMessage}</p>
+            ) : null}
+            {posts && posts.length > 0 ? (
+                posts.map((post) => {
+                    return <Posts.Post _id={post._id} key={`post-${post._id}`} />;
+                })
+            ) : (
+                <p className={styles["empty-message"]}>Nothing to see here!</p>
+            )}
             <div className={styles["create-new-post-button-wrapper"]}>
                 <div className={styles["create-new-post-button-container"]}>
                     <Buttons.Basic
