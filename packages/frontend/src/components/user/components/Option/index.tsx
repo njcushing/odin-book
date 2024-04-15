be29eea0b5bcb4fc88a2fa54c18cc5a62e35a6b7
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-15 22:16:48 +0100

    Optionally display 'Follow'/'Unfollow' button in user Option component
    If the component is displaying information about the active user, the button will not be displayed, as the active user cannot follow/unfollow themselves.

--- packages/frontend/src/components/user/components/Option/index.tsx
+++ packages/frontend/src/components/user/components/Option/index.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "@/context/user";
 import * as useAsync from "@/hooks/useAsync";
 import Buttons from "@/components/buttons";
 import mongoose from "mongoose";
@@ -16,9 +17,16 @@ export type OptionTypes = {
 };
 
 function Option({ _id, overrideOptionData }: OptionTypes) {
-    const [optionData, setOptionData] = useState<GetOptionResponse>(null);
+    const { user, extract } = useContext(UserContext);
+
+    const [displayFollowButton, setDisplayFollowButton] = useState<boolean>(true);
+
+    useEffect(() => {
+        setDisplayFollowButton(extract("_id") !== _id);
+    }, [user, _id, extract]);
 
     // get option api handling
+    const [optionData, setOptionData] = useState<GetOptionResponse>(null);
     const [getOptionResponse, setGetOptionParams, getOptionAgain] = useAsync.GET<
         GetOptionParams,
         GetOptionResponse
@@ -91,13 +99,15 @@ function Option({ _id, overrideOptionData }: OptionTypes) {
                         accountTag={optionData.accountTag}
                         size="s"
                     />
-                    <Buttons.Basic
-                        text={optionData.isFollowing ? "Unfollow" : "Follow"}
-                        symbol={optionData.isFollowing ? "person_remove" : "person_add"}
-                        palette={optionData.isFollowing ? "red" : "orange"}
-                        onClickHandler={() => followUserAgain(true)}
-                        otherStyles={{ fontSize: "1.0rem" }}
-                    />
+                    {displayFollowButton ? (
+                        <Buttons.Basic
+                            text={optionData.isFollowing ? "Unfollow" : "Follow"}
+                            symbol={optionData.isFollowing ? "person_remove" : "person_add"}
+                            palette={optionData.isFollowing ? "red" : "orange"}
+                            onClickHandler={() => followUserAgain(true)}
+                            otherStyles={{ fontSize: "1.0rem" }}
+                        />
+                    ) : null}
                 </div>
             )}
         </div>
