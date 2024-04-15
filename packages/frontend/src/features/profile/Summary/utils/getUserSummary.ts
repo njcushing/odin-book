e78b1d4b783a33eb448d9a89d68f3ee63b89df8a
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-15 21:09:02 +0100

    Create function for fetching user summary data from server

---
+++ packages/frontend/src/features/profile/Summary/utils/getUserSummary.ts
@@ -0,0 +1,72 @@
+import * as apiFunctionTypes from "@shared/utils/apiFunctionTypes";
+import mongoose from "mongoose";
+import saveTokenFromAPIResponse from "@/utils/saveTokenFromAPIResponse";
+
+export type Params = {
+    userId: mongoose.Types.ObjectId | null | undefined;
+};
+
+export type Response = {
+    _id: mongoose.Types.ObjectId | null | undefined;
+    accountTag: string;
+    githubId?: string;
+    followingCount: number;
+    followersCount: number;
+    postCount: number;
+    likesCount: number;
+    repliesCount: number;
+    preferences: {
+        displayName: string;
+        bio: string;
+        profileImage: {
+            _id: mongoose.Types.ObjectId | null | undefined;
+            url: string;
+        } | null;
+    };
+    isFollowing: boolean;
+    creationDate: string;
+} | null;
+
+const getUserSummary: apiFunctionTypes.GET<Params, Response> = async (
+    data,
+    abortController = null,
+) => {
+    const { userId } = data.params as Params;
+
+    if (!userId) {
+        return {
+            status: 400,
+            message: "No userId provided to route path",
+            data: null,
+        };
+    }
+
+    const result = await fetch(`${import.meta.env.VITE_SERVER_DOMAIN}/user/${userId}/summary`, {
+        signal: abortController ? abortController.signal : null,
+        method: "GET",
+        mode: "cors",
+        headers: {
+            Authorization: localStorage.getItem("odin-book-auth-token") || "",
+        },
+    })
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                data: responseJSON.data.user,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+                data: null,
+            };
+        });
+    return result;
+};
+
+export default getUserSummary;
