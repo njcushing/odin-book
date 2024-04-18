e86bf4761d94fa230ceae4a2b7ec9cf1f5b4cb37
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-18 10:59:19 +0100

    Give Skeleton accessibility component 'waiting' boolean prop
    This prop will cause the visual effects of the component to be displayed only when set to 'true'.

--- packages/frontend/src/components/accessibility/components/Skeleton/index.tsx
+++ packages/frontend/src/components/accessibility/components/Skeleton/index.tsx
@@ -2,15 +2,18 @@ import React, { ReactNode } from "react";
 import styles from "./index.module.css";
 
 type SkeletonTypes = {
+    waiting?: boolean;
     style?: React.CSSProperties;
     children?: ReactNode;
 };
 
-function Skeleton({ style, children }: SkeletonTypes) {
-    return (
+function Skeleton({ waiting = false, style, children }: SkeletonTypes) {
+    return waiting ? (
         <div style={style} className={children ? styles["parent"] : styles["container"]}>
             {children}
         </div>
+    ) : (
+        children
     );
 }
 
