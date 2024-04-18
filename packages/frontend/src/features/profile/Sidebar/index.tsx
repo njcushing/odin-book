e3f8f0bde577319d4e30df042322df5e50a3dadf
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-18 12:55:20 +0100

    Implement Skeleton accessibility component in profile Sidebar component

--- packages/frontend/src/features/profile/Sidebar/index.tsx
+++ packages/frontend/src/features/profile/Sidebar/index.tsx
@@ -1,8 +1,9 @@
-import { useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { UserContext } from "@/context/user";
 import createMultilineTextTruncateStyles from "@/utils/createMultilineTextTruncateStyles";
 import Images from "@/components/images";
 import User from "@/components/user";
+import Accessibility from "@/components/accessibility";
 import formatCreationDate from "@/utils/formatCreationDate";
 import styles from "./index.module.css";
 
@@ -11,7 +12,13 @@ type SidebarTypes = {
 };
 
 function Sidebar({ type }: SidebarTypes) {
-    const { extract } = useContext(UserContext);
+    const { awaitingResponse, extract } = useContext(UserContext);
+
+    const [waiting, setWaiting] = useState<boolean>(awaitingResponse);
+
+    useEffect(() => {
+        setWaiting(awaitingResponse);
+    }, [awaitingResponse]);
 
     if (type === "thin") {
         return (
@@ -25,37 +32,50 @@ function Sidebar({ type }: SidebarTypes) {
         <div className={styles["container"]}>
             <div className={styles["row-one"]}>
                 <User.ImageAndName
+                    waiting={waiting}
                     image={{ src: new Uint8Array([]), alt: "" }}
                     displayName={`${extract("preferences.displayName")}`}
                     accountTag={`${extract("accountTag")}`}
                     size="m"
                 />
             </div>
-            {`${extract("preferences.bio")}`.length > 0 ? (
-                <div className={styles["row-two"]}>
-                    <p
-                        className={styles["bio"]}
-                        style={{ ...createMultilineTextTruncateStyles(4) }}
-                    >
-                        {`${extract("preferences.bio")}`}
-                    </p>
-                </div>
+            {waiting || `${extract("preferences.bio")}`.length > 0 ? (
+                <Accessibility.Skeleton waiting={waiting} style={{ width: "100%" }}>
+                    <div className={styles["row-two"]}>
+                        <p
+                            className={styles["bio"]}
+                            style={{ ...createMultilineTextTruncateStyles(4) }}
+                        >
+                            {!waiting ? `${extract("preferences.bio")}` : "placeholder"}
+                        </p>
+                    </div>
+                </Accessibility.Skeleton>
             ) : null}
             <div className={styles["row-three"]}>
-                <p className={styles["following-count"]}>
-                    <strong>{`${extract("followingCount")}`}</strong> Following
-                </p>
-                <p className={styles["followers-count"]}>
-                    <strong>{`${extract("followersCount")}`}</strong> Followers
-                </p>
-                <p className={styles["likes-count"]}>
-                    <strong>{`${extract("likesCount")}`}</strong> Likes
-                </p>
+                <Accessibility.Skeleton waiting={waiting}>
+                    <p className={styles["following-count"]}>
+                        <strong>{`${extract("followingCount")}`}</strong> Following
+                    </p>
+                </Accessibility.Skeleton>
+                <Accessibility.Skeleton waiting={waiting}>
+                    <p className={styles["followers-count"]}>
+                        <strong>{`${extract("followersCount")}`}</strong> Followers
+                    </p>
+                </Accessibility.Skeleton>
+                <Accessibility.Skeleton waiting={waiting}>
+                    <p className={styles["likes-count"]}>
+                        <strong>{`${extract("likesCount")}`}</strong> Likes
+                    </p>
+                </Accessibility.Skeleton>
             </div>
             <div className={styles["row-four"]}>
-                <p className={styles["account-creation-date"]}>
-                    {formatCreationDate(`${extract("creationDate")}`)}
-                </p>
+                <Accessibility.Skeleton waiting={waiting}>
+                    <p className={styles["account-creation-date"]}>
+                        {!waiting
+                            ? formatCreationDate(`${extract("creationDate")}`)
+                            : "Account creation date unknown"}
+                    </p>
+                </Accessibility.Skeleton>
             </div>
         </div>
     );
