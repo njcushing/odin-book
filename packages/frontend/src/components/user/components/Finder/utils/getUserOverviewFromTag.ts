b56805b206c220baf7339aeccdaa69a66acd1724
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-19 10:30:02 +0100

    Write function for requesting user overview from server using account tag

---
+++ packages/frontend/src/components/user/components/Finder/utils/getUserOverviewFromTag.ts
@@ -0,0 +1,67 @@
+import * as apiFunctionTypes from "@shared/utils/apiFunctionTypes";
+import mongoose from "mongoose";
+import saveTokenFromAPIResponse from "@/utils/saveTokenFromAPIResponse";
+
+export type Params = {
+    accountTag: string;
+};
+
+export type Response = {
+    _id: mongoose.Types.ObjectId;
+    accountTag: string;
+    preferences: {
+        displayName: string;
+        profileImage?: {
+            _id: mongoose.Types.ObjectId;
+            url: string;
+            alt: string;
+        } | null;
+    };
+} | null;
+
+const getUserOverviewFromTag: apiFunctionTypes.GET<Params, Response> = async (
+    data,
+    abortController = null,
+) => {
+    const { accountTag } = data.params as Params;
+
+    const queryObject = { accountTag, softCheck: "true" };
+    const urlParams = new URLSearchParams();
+    Object.entries(queryObject).forEach(([key, value]) => {
+        if (value !== "" && value !== undefined && value !== null) {
+            urlParams.append(key, value);
+        }
+    });
+
+    const result = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/user/overviewFromTag?${urlParams}`,
+        {
+            signal: abortController ? abortController.signal : null,
+            method: "GET",
+            mode: "cors",
+            headers: {
+                Authorization: localStorage.getItem("odin-book-auth-token") || "",
+            },
+        },
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                data: responseJSON.data.user,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+                data: null,
+            };
+        });
+    return result;
+};
+
+export default getUserOverviewFromTag;
