6458775f55cbad30c897b8ce4885c3113ac50af1
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-19 09:34:58 +0100

    Add chat validators to validator index file

--- packages/shared/src/validation/index.ts
+++ packages/shared/src/validation/index.ts
@@ -1,9 +1,11 @@
 import * as user from "./user";
 import * as post from "./post";
+import * as chat from "./chat";
 
 const validation = {
     user,
     post,
+    chat,
 };
 
 export default validation;
