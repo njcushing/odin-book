9bb74c5c313fa18fcad3f6a13f4baa468ac8f464
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-20 12:42:11 +0100

    Add /chat/:chatId/overview GET route to chat routes index file

--- packages/backend/src/controllers/chat/index.ts
+++ packages/backend/src/controllers/chat/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import * as get from "./get";
 import * as create from "./create";
 
 const router = express.Router();
 
+router.get("/:chatId/overview", get.overview);
 router.post("/", create.regular);
 
 export default router;
