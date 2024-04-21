356603a537f021bd031c372a26be1ebf14c34d9c
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-21 11:27:33 +0100

    Route message field validators through main validators index file

--- packages/shared/src/validation/index.ts
+++ packages/shared/src/validation/index.ts
@@ -1,11 +1,13 @@
 import * as user from "./user";
 import * as post from "./post";
 import * as chat from "./chat";
+import * as message from "./message";
 
 const validation = {
     user,
     post,
     chat,
+    message,
 };
 
 export default validation;
