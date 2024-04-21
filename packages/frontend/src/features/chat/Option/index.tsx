a3ad6598aa21fd4d30d283e760212acefc706484
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-21 10:33:17 +0100

    Perform complete overhaul of chat Option component
    The component now utilises the 'getChatOverview' function to fetch the necessary information for the chat (the ObjectId reference for which is specified with the _id prop). Also, clicking a chat will now redirect the user to the correct URL path for that chat. I have also implemented the Skeleton component for some visual effects to convey the information that the component is awaiting a response from the server containing the information to be displayed for the chat.

--- packages/frontend/src/features/chat/Option/index.tsx
+++ packages/frontend/src/features/chat/Option/index.tsx
@@ -1,72 +1,150 @@
+import { useState, useEffect } from "react";
+import * as useAsync from "@/hooks/useAsync";
 import Images from "@/components/images";
-import { ProfileTypes } from "@/components/images/components/Profile";
 import Buttons from "@/components/buttons";
+import Accessibility from "@/components/accessibility";
+import mongoose from "mongoose";
 import combineParticipantNames from "../utils/combineParticipantNames";
+import getChatOverview, { Params, Response } from "./utils/getChatOverview";
 import styles from "./index.module.css";
 
-type RecentMessageTypes = {
-    author?: string;
-    text?: string;
-};
-
 type OptionTypes = {
-    name?: string;
-    participants?: string[];
-    image?: ProfileTypes;
-    recentMessage?: RecentMessageTypes;
-    onDeleteClickHandler?: ((event: React.MouseEvent<HTMLButtonElement>) => void) | null;
+    _id: mongoose.Types.ObjectId | undefined | null;
+    overrideOptionData?: Response;
+    skeleton?: boolean;
 };
 
-function Option({
-    name = "",
-    participants = [],
-    image,
-    recentMessage,
-    onDeleteClickHandler = null,
-}: OptionTypes) {
+function Option({ _id, overrideOptionData, skeleton = false }: OptionTypes) {
+    const [waiting, setWaiting] = useState<boolean>(true);
+
+    // get option api handling
+    const [chatData, setChatData] = useState<Response>(null);
+    const [
+        getChatOverviewResponse /* setGetChatOverviewParams */,
+        ,
+        getChatOverviewAgain,
+        gettingChatOverview,
+    ] = useAsync.GET<Params, Response>(
+        {
+            func: getChatOverview,
+            parameters: [{ params: { chatId: _id } }, null],
+        },
+        !overrideOptionData,
+    );
+    useEffect(() => {
+        if (!overrideOptionData) {
+            const newState = getChatOverviewResponse ? getChatOverviewResponse.data : null;
+            setChatData(newState);
+        }
+    }, [overrideOptionData, getChatOverviewResponse]);
+
+    // error message handling
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    useEffect(() => {
+        if (
+            getChatOverviewResponse &&
+            getChatOverviewResponse.status >= 400 &&
+            getChatOverviewResponse.message &&
+            getChatOverviewResponse.message.length > 0
+        ) {
+            setErrorMessage(getChatOverviewResponse.message);
+        }
+    }, [getChatOverviewResponse]);
+
+    // fetch option again
+    useEffect(() => {
+        if (overrideOptionData) {
+            setChatData(overrideOptionData);
+        } else {
+            getChatOverviewAgain(true);
+        }
+    }, [overrideOptionData, getChatOverviewAgain]);
+
+    useEffect(() => {
+        setWaiting(gettingChatOverview);
+    }, [gettingChatOverview]);
+
     let chatName = "Chat";
-    if (name && name.length > 0) {
-        chatName = name;
-    } else if (participants && participants.length > 0) {
-        chatName = combineParticipantNames(participants, 3);
+    if (chatData) {
+        if (chatData.name.length > 0) {
+            chatName = chatData.name;
+        } else if (chatData.participants && chatData.participants.length > 0) {
+            const extractedParticipantNames = chatData.participants.map((participant) => {
+                if (participant.nickname.length > 0) {
+                    return participant.nickname;
+                }
+                if (participant.user.preferences.displayName.length > 0) {
+                    return participant.user.preferences.displayName;
+                }
+                return participant.user.accountTag;
+            });
+            chatName = combineParticipantNames(extractedParticipantNames as unknown as string[], 3);
+        }
+    }
+
+    let recentMessage = "";
+    if (chatData && chatData.recentMessage) {
+        recentMessage = `${chatData.recentMessage.author}: ${chatData.recentMessage.text}`;
+    } else if (waiting) {
+        recentMessage = "placeholder";
     }
 
     return (
-        <div className={styles["container"]}>
-            <div className={styles["profile-image-container"]}>
-                <Images.Profile
-                    src={image && image.src}
-                    alt={image && image.alt}
-                    status={image && image.status}
-                    sizePx={64}
-                />
-            </div>
-            <div className={styles["name-and-message-container"]}>
-                <p className={`truncate-ellipsis ${styles["name"]}`} aria-label="chat name">
-                    {chatName}
-                </p>
-                {recentMessage ? (
-                    <p
-                        className={`truncate-ellipsis ${styles["message"]}`}
-                        aria-label="recent message"
-                    >
-                        {`${recentMessage.author}: ${recentMessage.text}`}
-                    </p>
-                ) : null}
-            </div>
-            <Buttons.Basic
-                text=""
-                symbol="delete"
-                onClickHandler={(e) => {
-                    if (onDeleteClickHandler) onDeleteClickHandler(e);
-                }}
-                palette="red"
-                otherStyles={{
-                    fontSize: "0.9rem",
-                    padding: "0.5rem",
-                }}
-            />
-        </div>
+        <a className={styles["container"]} href={`/chat/${_id}`}>
+            {errorMessage.length > 0 ? (
+                <p className={styles["error-message"]}>{errorMessage}</p>
+            ) : null}
+            {skeleton || chatData ? (
+                <>
+                    <div className={styles["profile-image-container"]}>
+                        <Accessibility.Skeleton
+                            waiting={waiting}
+                            style={{ borderRadius: "9999px" }}
+                        >
+                            <Images.Profile
+                                src={chatData && chatData.image ? chatData.image.url : ""}
+                                alt={chatData && chatData.image ? chatData.image.alt : ""}
+                                sizePx={64}
+                            />
+                        </Accessibility.Skeleton>
+                    </div>
+                    <div className={styles["name-and-message-container"]}>
+                        <Accessibility.Skeleton waiting={waiting} style={{ width: "100%" }}>
+                            <p
+                                className={`truncate-ellipsis ${styles["name"]}`}
+                                aria-label="chat name"
+                            >
+                                {chatName}
+                            </p>
+                        </Accessibility.Skeleton>
+                        <Accessibility.Skeleton waiting={waiting} style={{ width: "100%" }}>
+                            {recentMessage.length > 0 ? (
+                                <p
+                                    className={`truncate-ellipsis ${styles["message"]}`}
+                                    aria-label="recent message"
+                                >
+                                    {recentMessage}
+                                </p>
+                            ) : null}
+                        </Accessibility.Skeleton>
+                    </div>
+                    <Accessibility.Skeleton waiting={waiting} style={{ borderRadius: "9999px" }}>
+                        <Buttons.Basic
+                            text=""
+                            symbol="delete"
+                            onClickHandler={(e) => {
+                                // delete chat
+                            }}
+                            palette="red"
+                            otherStyles={{
+                                fontSize: "0.9rem",
+                                padding: "0.5rem",
+                            }}
+                        />
+                    </Accessibility.Skeleton>
+                </>
+            ) : null}
+        </a>
     );
 }
 
