cfd9fc8c6ad53e19c644c736af424afa6def61e6
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-23 21:06:15 +0100

    Route /chat/:chatId/message/:messageId GET route through index file

--- packages/backend/src/controllers/chat/index.ts
+++ packages/backend/src/controllers/chat/index.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get("/:chatId/overview", get.overview);
 router.get("/:chatId/messages", get.messages);
+router.get("/:chatId/message/:messageId", get.message);
 router.post("/", create.regular);
 
 export default router;
