98038225236b73b25f6bf8a38c7a21a8edc91f69
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-24 21:51:27 +0100

    Overhaul Active chat component
    The component is now a context provider that contains some important information about the active chat including the name, and participants' information. It utilises the 'getChatOverview' function to fetch this information from the server. I have also moved the 'message list' part of the component to its own component (MessageList).

--- packages/frontend/src/features/chat/Active/index.tsx
+++ packages/frontend/src/features/chat/Active/index.tsx
@@ -1,95 +1,154 @@
-import { useState } from "react";
-import LayoutUI from "@/layouts";
+import { createContext, useState, useEffect, useMemo } from "react";
+import { useParams } from "react-router-dom";
+import * as useAsync from "@/hooks/useAsync";
+import mongoose from "mongoose";
 import Buttons from "@/components/buttons";
 import Inputs from "@/components/inputs";
-import * as extendedTypes from "@shared/utils/extendedTypes";
-import * as mockData from "@/mockData";
+import Accessibility from "@/components/accessibility";
+import getChatOverview, { Params, Response } from "@/features/chat/utils/getChatOverview";
+import extractParticipantsInformation, {
+    ReturnTypes as extractedParticipantsInfo,
+} from "./utils/extractParticipantsInformation";
 import Chat from "..";
 import styles from "./index.module.css";
 
-function Active() {
-    const [replyingTo, setReplyingTo]: [string, extendedTypes.Setter<string>] = useState("");
+type ActiveTypes = {
+    _id?: mongoose.Types.ObjectId;
+    getIdFromURLParam?: boolean;
+};
 
-    const messages = mockData.messages(16);
+interface ChatState {
+    chatData: Response;
+    participantsInfo: extractedParticipantsInfo;
+    awaitingResponse: boolean;
+}
 
-    const messageList = (
-        <div className={styles["message-list-container"]} key={0}>
-            <LayoutUI.List
-                label="navigation"
-                ordered={false}
-                listItems={messages.map((message, i) => message)}
-                scrollable
-                listStyles={{
-                    flexDirection: "column",
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    flexWrap: "nowrap",
-                    gap: "0.4rem",
-                    width: "calc(100% - (2 * 0.4rem))",
-                    height: "auto",
-                    padding: "0.4rem",
-                    margin: "0rem",
-                }}
-            />
-        </div>
+const defaultState: ChatState = {
+    chatData: null,
+    participantsInfo: {},
+    awaitingResponse: true,
+};
+
+export const ChatContext = createContext<ChatState>(defaultState);
+
+function Active({ _id, getIdFromURLParam = false }: ActiveTypes) {
+    const { chatId } = useParams();
+
+    const [replyingTo, setReplyingTo] = useState<string>("");
+
+    const [waiting, setWaiting] = useState(true);
+
+    const [chatData, setChatData] = useState<Response>(null);
+    const [response, setParams, setAttempting, gettingChatOverview] = useAsync.GET<
+        Params,
+        Response
+    >(
+        {
+            func: getChatOverview,
+            parameters: [
+                {
+                    params: {
+                        chatId: !getIdFromURLParam
+                            ? _id
+                            : (chatId as unknown as mongoose.Types.ObjectId),
+                    },
+                },
+                null,
+            ],
+        },
+        true,
     );
+    const [participantsInfo, setParticipantsInfo] = useState<extractedParticipantsInfo>({});
+    const [errorMessage, setErrorMessage] = useState<string>("");
 
-    const replyingToElement =
-        replyingTo.length > 0 ? (
-            <div className={styles["replying-to-container"]} key={0}>
-                <Buttons.Basic
-                    text=""
-                    symbol="cancel"
-                    onClickHandler={() => setReplyingTo("")}
-                    otherStyles={{ fontSize: "1.2rem", padding: "0.5rem" }}
-                />
-                <p className={`truncate-ellipsis ${styles["replying-to-string"]}`} key={0}>
-                    {`Replying to ${replyingTo}`}
-                </p>
-            </div>
-        ) : null;
+    useEffect(() => {
+        const newState = response ? response.data : null;
+        setChatData(newState);
+    }, [response]);
 
-    const messageBox = (
-        <div className={styles["message-box-container"]} key={0}>
-            <Inputs.Message
-                textFieldId="message-text"
-                textFieldName="messageText"
-                placeholder="Type your message..."
-                imageFieldId="message-images"
-                imageFieldName="messageImages"
-            />
-        </div>
-    );
+    useEffect(() => {
+        setParticipantsInfo(chatData ? extractParticipantsInformation(chatData.participants) : {});
+    }, [chatData]);
+
+    useEffect(() => {
+        setAttempting(true);
+        setErrorMessage("");
+        setParams([
+            {
+                params: {
+                    chatId: !getIdFromURLParam
+                        ? _id
+                        : (chatId as unknown as mongoose.Types.ObjectId),
+                },
+            },
+            null,
+        ]);
+    }, [_id, getIdFromURLParam, chatId, setParams, setAttempting]);
+
+    if (response && response.status === 401) window.location.assign("/");
+
+    useEffect(() => {
+        if (response && response.status >= 400 && response.message && response.message.length > 0) {
+            setErrorMessage(response.message);
+        } else {
+            setErrorMessage("");
+        }
+    }, [response]);
+
+    useEffect(() => {
+        setWaiting(gettingChatOverview);
+    }, [gettingChatOverview]);
 
     return (
-        <div className={styles["container"]}>
-            <LayoutUI.Spatial
-                width="100%"
-                height="100%"
-                arrangements={[
-                    {
-                        type: "rows",
-                        minWidth: 0,
-                        maxWidth: 999999,
-                        minHeight: 0,
-                        maxHeight: 999999,
-                        areas: [
-                            { size: "auto", children: [<Chat.Header key={0} />] },
-                            { size: "1fr", children: [messageList] },
-                            { size: "auto", children: [replyingToElement] },
-                            { size: "auto", children: [messageBox] },
-                        ],
-                        style: {
-                            justifySelf: "flex-start",
-                            alignSelf: "center",
-                            width: "100%",
-                            height: "100%",
-                            padding: "0rem",
-                        },
-                    },
-                ]}
-            />
-        </div>
+        <ChatContext.Provider
+            value={useMemo(
+                () => ({ chatData, participantsInfo, awaitingResponse: waiting }),
+                [chatData, participantsInfo, waiting],
+            )}
+        >
+            {!waiting ? (
+                <div className={styles["container"]}>
+                    {errorMessage.length > 0 ? (
+                        <p className={styles["error-message"]}>{errorMessage}</p>
+                    ) : null}
+                    {chatData ? (
+                        <div className={styles["chat-content-container"]}>
+                            <Chat.Header />
+                            <Chat.MessageList getIdFromURLParam />
+                            {replyingTo.length > 0 ? (
+                                <div className={styles["replying-to-container"]} key={0}>
+                                    <Buttons.Basic
+                                        text=""
+                                        symbol="cancel"
+                                        onClickHandler={() => setReplyingTo("")}
+                                        otherStyles={{ fontSize: "1.2rem", padding: "0.5rem" }}
+                                    />
+                                    <p
+                                        className={`truncate-ellipsis ${styles["replying-to-string"]}`}
+                                        key={0}
+                                    >
+                                        {`Replying to ${replyingTo}`}
+                                    </p>
+                                </div>
+                            ) : null}
+                            <div className={styles["message-box-container"]} key={0}>
+                                <Inputs.Message
+                                    textFieldId="message-text"
+                                    textFieldName="messageText"
+                                    placeholder="Type your message..."
+                                    imageFieldId="message-images"
+                                    imageFieldName="messageImages"
+                                />
+                            </div>
+                        </div>
+                    ) : (
+                        <p className={styles["empty-message"]}>Nothing to see here!</p>
+                    )}
+                </div>
+            ) : (
+                <Accessibility.WaitingWheel />
+            )}
+        </ChatContext.Provider>
     );
 }
 
