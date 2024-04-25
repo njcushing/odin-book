b024db4f197bb06615d0818c038eefae974effd3
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-25 11:20:01 +0100

    Write function for requesting a chat's message be deleted

---
+++ packages/frontend/src/features/chat/Message/utils/deleteChatMessage.ts
@@ -0,0 +1,66 @@
+import * as apiFunctionTypes from "@shared/utils/apiFunctionTypes";
+import mongoose from "mongoose";
+import saveTokenFromAPIResponse from "@/utils/saveTokenFromAPIResponse";
+
+export type Params = {
+    chatId: mongoose.Types.ObjectId | null | undefined;
+    messageId: mongoose.Types.ObjectId | null | undefined;
+};
+
+export type Response = null;
+
+const deleteChatMessage: apiFunctionTypes.DELETE<Params, Response> = async (
+    data,
+    abortController = null,
+) => {
+    const { chatId } = data.params as Params;
+    const { messageId } = data.params as Params;
+
+    if (!chatId) {
+        return {
+            status: 400,
+            message: "No chatId provided to route path",
+            data: null,
+        };
+    }
+
+    if (!messageId) {
+        return {
+            status: 400,
+            message: "No messageId provided to route path",
+            data: null,
+        };
+    }
+
+    const result = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/chat/${chatId}/message/${messageId}`,
+        {
+            signal: abortController ? abortController.signal : null,
+            method: "DELETE",
+            mode: "cors",
+            headers: {
+                Authorization: localStorage.getItem("odin-book-auth-token") || "",
+            },
+        },
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                data: null,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+                data: null,
+            };
+        });
+    return result;
+};
+
+export default deleteChatMessage;
