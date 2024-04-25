cf1632f36e142c8f838e9209578fa1902f5b037d
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-25 12:12:27 +0100

    Route /chat/:chatId/message/:messageId DELETE route through index file

--- packages/backend/src/controllers/chat/index.ts
+++ packages/backend/src/controllers/chat/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as get from "./get";
 import * as create from "./create";
+import * as remove from "./remove";
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.get("/:chatId/messages", get.messages);
 router.get("/:chatId/message/:messageId", get.message);
 router.post("/", create.regular);
 router.post("/:chatId/message", create.message);
+router.delete("/:chatId/message/:messageId", remove.message);
 
 export default router;
