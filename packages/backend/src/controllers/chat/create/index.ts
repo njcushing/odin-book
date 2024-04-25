ed8080c56b7cc8b8b7d62b3d31b710aa32f94278
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-25 10:05:33 +0100

    Write functionality for /chat/:chatId/message POST route

--- packages/backend/src/controllers/chat/create/index.ts
+++ packages/backend/src/controllers/chat/create/index.ts
@@ -4,10 +4,15 @@ import asyncHandler from "express-async-handler";
 import generateToken from "@/utils/generateToken";
 import sendResponse from "@/utils/sendResponse";
 import protectedRouteJWT from "@/utils/protectedRouteJWT";
+import { multiple } from "@/utils/uploadImage";
 import User from "@/models/user";
 import Chat from "@/models/chat";
+import Message from "@/models/message";
+import Image from "@/models/image";
 import * as types from "@/utils/types";
+import { body } from "express-validator";
 import checkRequestValidationError from "@/utils/checkRequestValidationError";
+import checkUserAuthorisedInChat from "../utils/checkUserAuthorisedInChat";
 import validators from "../validators";
 
 export const regular = [
@@ -151,4 +156,140 @@ export const regular = [
             }
         });
     }),
+];
+
+export const message = [
+    protectedRouteJWT,
+    validators.param.chatId,
+    validators.body.text,
+    validators.body.images,
+    validators.body.replyingTo,
+    body("text").custom((value, { req }) => {
+        if (req.body.text.length === 0 && req.body.images.length === 0) {
+            throw new Error(
+                "Your message must not be empty; there must either be some text, or images.",
+            );
+        } else {
+            return true;
+        }
+    }),
+    checkRequestValidationError,
+    async (req: Request, res: Response) => {
+        const { chatId } = req.params;
+
+        // find user & chat
+        const user = await User.findById(res.locals.user._id);
+        const chat = await Chat.findById(chatId);
+        if (!user) return sendResponse(res, 404, "User not found in database");
+        if (!chat) return sendResponse(res, 404, "Chat not found in database");
+
+        // validate user is allowed to post in chat
+        const [userAuthorised, authMessage] = checkUserAuthorisedInChat(
+            user._id,
+            chat.participants,
+            true,
+            "guest",
+        );
+        if (!userAuthorised) return sendResponse(res, 401, authMessage);
+
+        // attempt to upload all images
+        const [uploadResult, uploadResponses] = await multiple(req.body.images);
+        if (!uploadResult) return sendResponse(res, 500, "Could not upload images");
+
+        const session = await mongoose.startSession();
+
+        // delete all created documents in case of aborted transaction
+        session.on("abort", async () => {
+            try {
+                await Message.deleteMany({ session });
+                await Image.deleteMany({ session });
+            } catch (error) {
+                console.error("Error occurred while deleting documents:", error);
+            }
+        });
+
+        try {
+            session.startTransaction();
+
+            // create images
+            const images = await Promise.all(
+                uploadResponses.map(async (url) => {
+                    const image = new Image({ url });
+                    await image
+                        .save({ session })
+                        .then(async (result) => result)
+                        .catch((saveErr) => {
+                            throw saveErr;
+                        });
+                    return image;
+                }),
+            )
+                .then((result) => result)
+                .catch((err) => {
+                    throw err;
+                });
+
+            // create message
+            const newMessage = new Message({
+                author: user._id,
+                text: req.body.text,
+                images: images.map((image) => image._id),
+                replyingTo: req.body.replyingTo ? req.body.replyingTo : null,
+            });
+            await newMessage.save({ session }).catch((saveErr) => {
+                const error = new Error(saveErr);
+                throw error;
+            });
+
+            // add new message _id to chat's 'messages' array field
+            const updatedChat = await Chat.updateOne(
+                { _id: chat._id },
+                { $addToSet: { messages: newMessage._id } },
+            );
+            if (!updatedChat.acknowledged) {
+                const error = new Error(
+                    `Could not add message to chat's messages.`,
+                ) as types.ResponseError;
+                error.status = 500;
+                throw error;
+            }
+
+            await session.commitTransaction();
+
+            session.endSession();
+
+            // create token and send response
+            const response = await generateToken(res.locals.user)
+                .then((token) => {
+                    return sendResponse(res, 201, "Message created successfully", {
+                        token,
+                        newMessage,
+                    });
+                })
+                .catch((tokenErr) => {
+                    return sendResponse(
+                        res,
+                        500,
+                        tokenErr.message ||
+                            `Message created successfully, but token creation failed`,
+                        { newMessage },
+                        tokenErr,
+                    );
+                });
+            return response;
+        } catch (err: unknown) {
+            await session.abortTransaction();
+
+            session.endSession();
+
+            const status =
+                err && typeof err === "object" && "status" in err ? (err.status as number) : 500;
+            const errMessage =
+                err && typeof err === "object" && "message" in err
+                    ? (err.message as string)
+                    : "Internal Server Error";
+            const error = err instanceof Error ? (err as types.ResponseError) : null;
+            return sendResponse(res, status, errMessage, null, error);
+        }
+    },
 ];
