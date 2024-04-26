be82c7ad0a5b0cc7d44c54fc7b5dd5f5073afe4b
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-26 10:41:01 +0100

    Write function to request a chat's name is changed

---
+++ packages/frontend/src/features/chat/Header/utils/changeChatName.ts
@@ -0,0 +1,67 @@
+import * as apiFunctionTypes from "@shared/utils/apiFunctionTypes";
+import mongoose from "mongoose";
+import saveTokenFromAPIResponse from "@/utils/saveTokenFromAPIResponse";
+
+export type Params = {
+    chatId: mongoose.Types.ObjectId | null | undefined;
+};
+
+export type Body = {
+    name: string;
+};
+
+export type Response = null;
+
+const changeChatName: apiFunctionTypes.PUT<Params, Body, Response> = async (
+    data,
+    abortController = null,
+) => {
+    const { chatId } = data.params as Params;
+
+    if (!chatId) {
+        return {
+            status: 400,
+            message: "No chatId provided to route path",
+            data: null,
+        };
+    }
+
+    let body;
+    if (data && data.body) body = data.body;
+
+    let name = "";
+    if (body) {
+        name = body.name;
+    }
+
+    const result = await fetch(`${import.meta.env.VITE_SERVER_DOMAIN}/chat/${chatId}/name`, {
+        signal: abortController ? abortController.signal : null,
+        method: "PUT",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: localStorage.getItem("odin-book-auth-token") || "",
+        },
+        body: JSON.stringify({ name }),
+    })
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                data: null,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+                data: null,
+            };
+        });
+    return result;
+};
+
+export default changeChatName;
