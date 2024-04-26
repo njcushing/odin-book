916fa6a898b2e6651f3cb172ae40158894d7dfd7
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-26 12:53:29 +0100

    Utilise updateChatName function in chat Header component
    A request can now be made for the chat's name to be changed from the component. I have also changed the options when the name is being edited; there are now two buttons: a 'done' button and a 'close' button. An error message will now display if there are any errors as a result of the request. The style rules for the component have also been updated.

--- packages/frontend/src/features/chat/Header/index.tsx
+++ packages/frontend/src/features/chat/Header/index.tsx
@@ -1,62 +1,171 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { ChatContext } from "@/features/chat/Active";
+import * as useAsync from "@/hooks/useAsync";
+import Images from "@/components/images";
 import Buttons from "@/components/buttons";
 import determineChatName from "@/features/chat/utils/determineChatName";
+import validation from "@shared/validation";
+import createMultilineTextTruncateStyles from "@/utils/createMultilineTextTruncateStyles";
+import changeChatName, { Params, Body, Response } from "./utils/changeChatName";
 import styles from "./index.module.css";
 
 const buttonStyles = { fontSize: "1.1rem", padding: "0.6rem" };
 
 type HeaderTypes = {
     overrideChatName?: string;
-    onEditNameHandler?: ((name: string) => void) | null;
 };
 
-function Header({ overrideChatName, onEditNameHandler = null }: HeaderTypes) {
+function Header({ overrideChatName }: HeaderTypes) {
     const { chatData } = useContext(ChatContext);
 
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [waiting, setWaiting] = useState<boolean>(false);
     const [editingName, setEditingName] = useState<boolean>(false);
-    const [chatName, setChatName] = useState<string>(
-        overrideChatName || determineChatName(chatData),
-    );
+    const [doneButtonClicked, setDoneButtonClicked] = useState<boolean>(false);
     const [nameStored, setNameStored] = useState<string>(
         overrideChatName || determineChatName(chatData),
     );
 
+    const [chatName, setChatName] = useState<string>(
+        overrideChatName || determineChatName(chatData),
+    );
+    const [response, setParams, setAttempting, settingChatName] = useAsync.PUT<
+        Params,
+        Body,
+        Response
+    >(
+        {
+            func: changeChatName,
+            parameters: [
+                {
+                    params: { chatId: chatData && chatData._id },
+                    body: { name: chatName },
+                },
+                null,
+            ],
+        },
+        false,
+    );
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
     useEffect(() => {
-        if (!editingName && onEditNameHandler && chatName !== nameStored) {
-            onEditNameHandler(chatName);
-            setNameStored(chatName);
+        if (doneButtonClicked && chatName !== nameStored) {
+            const valid = validation.chat.name(chatName, "front");
+            if (valid.status) {
+                setParams([
+                    { params: { chatId: chatData && chatData._id }, body: { name: chatName } },
+                    null,
+                ]);
+                setAttempting(true);
+            } else {
+                setChatName(nameStored);
+                if (inputRef.current) {
+                    inputRef.current.value = nameStored;
+                }
+                setErrorMessage(valid.message);
+            }
         }
-    }, [editingName, chatName, onEditNameHandler, nameStored]);
+        setDoneButtonClicked(false);
+    }, [doneButtonClicked, chatName, nameStored, chatData, setParams, setAttempting]);
+
+    useEffect(() => {
+        if (response) {
+            if (response.status >= 400 && response.message && response.message.length > 0) {
+                setErrorMessage(response.message);
+                setChatName(nameStored);
+            } else {
+                setErrorMessage("");
+                if (chatName.length === 0) {
+                    setChatName(determineChatName(chatData));
+                    setNameStored(determineChatName(chatData));
+                } else {
+                    setNameStored(chatName);
+                }
+                setEditingName(false);
+            }
+        }
+    }, [response, chatData, chatName, nameStored]);
+
+    useEffect(() => {
+        setWaiting(settingChatName);
+    }, [settingChatName]);
 
     return (
-        <div className={styles["header"]}>
-            <div className={styles["name-container"]}>
-                {!editingName ? (
-                    <h3 className={`truncate-ellipsis ${styles["name"]}`}>{chatName}</h3>
-                ) : (
-                    <input
-                        className={styles["edit-name"]}
-                        type="text"
-                        defaultValue={chatName}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                            setChatName(e.target.value)
-                        }
-                        onBlur={() => setEditingName(false)}
-                        // eslint-disable-next-line jsx-a11y/no-autofocus
-                        autoFocus
-                    ></input>
-                )}
-                <Buttons.Basic
-                    text=""
-                    symbol={!editingName ? "edit" : "done"}
-                    label="edit chat name"
-                    onClickHandler={() => {
-                        setEditingName(!editingName);
-                    }}
-                    palette={!editingName ? "primary" : "green"}
-                    otherStyles={{ ...buttonStyles }}
+        <div className={styles["container"]}>
+            <div className={styles["chat-image-container"]}>
+                <Images.Profile
+                    src={chatData && chatData.image ? chatData.image.url : ""}
+                    alt={chatData && chatData.image ? chatData.image.alt : ""}
+                    sizePx={64}
                 />
+            </div>
+            <div className={styles["name-container"]}>
+                <div className={styles["name-container-top-row"]}>
+                    {!editingName ? (
+                        <>
+                            <h3 className={`truncate-ellipsis ${styles["name"]}`}>{chatName}</h3>
+                            <Buttons.Basic
+                                text=""
+                                symbol="edit"
+                                label="edit chat name"
+                                onClickHandler={() => setEditingName(true)}
+                                disabled={waiting}
+                                palette="primary"
+                                otherStyles={{ ...buttonStyles }}
+                            />
+                        </>
+                    ) : (
+                        <>
+                            <input
+                                className={styles["edit-name"]}
+                                type="text"
+                                defaultValue={chatName}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                    setChatName(e.target.value)
+                                }
+                                disabled={waiting}
+                                // eslint-disable-next-line jsx-a11y/no-autofocus
+                                autoFocus
+                                ref={inputRef}
+                            ></input>
+                            <div className={styles["edit-chat-name-options"]}>
+                                <Buttons.Basic
+                                    text=""
+                                    symbol="done"
+                                    label="confirm new chat name"
+                                    onClickHandler={() => {
+                                        if (chatName === nameStored) setEditingName(false);
+                                        else setDoneButtonClicked(true);
+                                    }}
+                                    disabled={waiting}
+                                    palette="green"
+                                    otherStyles={{ ...buttonStyles }}
+                                />
+                                <Buttons.Basic
+                                    text=""
+                                    symbol="close"
+                                    label="cancel editing chat name"
+                                    onClickHandler={() => {
+                                        setChatName(nameStored);
+                                        setEditingName(false);
+                                    }}
+                                    disabled={waiting}
+                                    palette="red"
+                                    otherStyles={{ ...buttonStyles }}
+                                />
+                            </div>
+                        </>
+                    )}
+                </div>
+                {errorMessage.length > 0 ? (
+                    <p
+                        className={styles["error-message"]}
+                        style={{ ...createMultilineTextTruncateStyles(3) }}
+                    >
+                        {errorMessage}
+                    </p>
+                ) : null}
             </div>
             <div className={styles["options"]}>
                 <Buttons.Basic
