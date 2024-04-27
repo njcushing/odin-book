a2e3b3664f5200f74b3b1f26f22e172181fdceca
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-27 12:34:21 +0100

    Write functionality for /chat/:chatId/participants PUT route

--- packages/backend/src/controllers/chat/put/index.ts
+++ packages/backend/src/controllers/chat/put/index.ts
@@ -64,3 +64,137 @@ export const name = [
         return response;
     },
 ];
+
+export const participants = [
+    protectedRouteJWT,
+    validators.param.chatId,
+    validators.body.participants,
+    checkRequestValidationError,
+    async (req: Request, res: Response) => {
+        const { chatId } = req.params;
+        const newParticipants = req.body.participants;
+
+        // validate all users specified
+        let invalidParticipant = false;
+        const participantValidationResult = await Promise.all(
+            newParticipants.map(async (participant: string) => {
+                const user = await User.findById(participant).exec();
+                if (!user) {
+                    throw new Error(`User not found in database: ${participant}`);
+                }
+                return user;
+            }),
+        ).catch((err) => {
+            invalidParticipant = true;
+            return sendResponse(res, 404, err.message, null, err);
+        });
+        if (invalidParticipant) return participantValidationResult;
+
+        // match chat to ensure active user is a participant in the specified chat
+        // also prevent a match if any participants are present in the 'participants' array
+        const chat = await Chat.findOne({
+            _id: chatId,
+            participants: {
+                $elemMatch: { user: res.locals.user.id },
+                $not: { $elemMatch: { user: { $in: newParticipants } } },
+            },
+        });
+        if (!chat) {
+            return sendResponse(
+                res,
+                404,
+                `Either chat could not be found in the database, user is not a participant in the
+                 specified chat, or user is attempting to add participants to the chat that are
+                 already in the chat`,
+            );
+        }
+
+        // validate active user is allowed to add new participants to the chat
+        const [userAuthorised, authMessage] = checkUserAuthorisedInChat(
+            res.locals.user.id,
+            chat.participants,
+            true,
+            "guest",
+        );
+        if (!userAuthorised) return sendResponse(res, 401, authMessage);
+
+        const session = await mongoose.startSession();
+        try {
+            session.startTransaction();
+
+            // add participants to chat's 'participants' array field
+            const updatedChat = await Chat.updateOne(
+                { _id: chat._id },
+                {
+                    $addToSet: {
+                        participants: {
+                            $each: newParticipants.map((participant: string) => ({
+                                user: participant,
+                            })),
+                        },
+                    },
+                },
+            );
+            if (!updatedChat.acknowledged) {
+                const error = new Error(
+                    `Could not add message to chat's messages.`,
+                ) as types.ResponseError;
+                error.status = 500;
+                throw error;
+            }
+
+            // add chat _id to 'chats' array for all new participants
+            await Promise.all(
+                newParticipants.map(async (participant: string) => {
+                    const updatedUser = await User.findByIdAndUpdate(participant, {
+                        $addToSet: { chats: chat._id },
+                    });
+                    if (updatedUser === null) {
+                        const error = new Error(
+                            `User not found in database: ${participant}.`,
+                        ) as types.ResponseError;
+                        error.status = 404;
+                        throw error;
+                    }
+                    return updatedUser;
+                }),
+            );
+
+            await session.commitTransaction();
+
+            session.endSession();
+
+            // create token and send response
+            const response = await generateToken(res.locals.user)
+                .then((token) => {
+                    return sendResponse(res, 200, "Participants successfully added to chat", {
+                        token,
+                    });
+                })
+                .catch((tokenErr) => {
+                    return sendResponse(
+                        res,
+                        500,
+                        tokenErr.message ||
+                            "Participants successfully added to chat, but token creation failed",
+                        null,
+                        tokenErr,
+                    );
+                });
+            return response;
+        } catch (err: unknown) {
+            await session.abortTransaction();
+
+            session.endSession();
+
+            const status =
+                err && typeof err === "object" && "status" in err ? (err.status as number) : 500;
+            const errMessage =
+                err && typeof err === "object" && "message" in err
+                    ? (err.message as string)
+                    : "Internal Server Error";
+            const error = err instanceof Error ? (err as types.ResponseError) : null;
+            return sendResponse(res, status, errMessage, null, error);
+        }
+    },
+];
