fc6c6386b75f31ac57d1f76264fa43862fa8c314
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-27 11:04:16 +0100

    Write function to request new participants be added to a chat

---
+++ packages/frontend/src/features/chat/AddUsers/utils/addParticipantsToChat.ts
@@ -0,0 +1,70 @@
+import * as apiFunctionTypes from "@shared/utils/apiFunctionTypes";
+import mongoose from "mongoose";
+import saveTokenFromAPIResponse from "@/utils/saveTokenFromAPIResponse";
+
+export type Params = {
+    chatId: mongoose.Types.ObjectId | null | undefined;
+};
+
+export type Body = {
+    participants: mongoose.Types.ObjectId[];
+};
+
+export type Response = null;
+
+const addParticipantsToChat: apiFunctionTypes.PUT<Params, Body, Response> = async (
+    data,
+    abortController = null,
+) => {
+    const { chatId } = data.params as Params;
+
+    if (!chatId) {
+        return {
+            status: 400,
+            message: "No chatId provided to route path",
+            data: null,
+        };
+    }
+
+    let body;
+    if (data && data.body) body = data.body;
+
+    let participants: mongoose.Types.ObjectId[] = [];
+    if (body) {
+        participants = body.participants;
+    }
+
+    const result = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/chat/${chatId}/participants`,
+        {
+            signal: abortController ? abortController.signal : null,
+            method: "PUT",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: localStorage.getItem("odin-book-auth-token") || "",
+            },
+            body: JSON.stringify({ participants }),
+        },
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                data: null,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+                data: null,
+            };
+        });
+    return result;
+};
+
+export default addParticipantsToChat;
