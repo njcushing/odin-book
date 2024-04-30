d585758062550c1e2138e3414fe779bbb6e4d9f1
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-30 22:32:43 +0100

    Write functionality for requesting additional posts when scrolling
    I have also adjusted the layout for the error message and create new post button elements such that there is available space for them at the top and bottom of the posts list, respectively. For example, the error message is displayed at the top of the list, but since it is sticky, it covers the top of the first post in the list, so I have created a buffer equal to the size of the element at the top of the list for it to be positioned when at the top of the scrollable area. This way, it will not cover the first post in the list. I've done the same for the create new post button and the last post in the list.

--- packages/frontend/src/features/posts/List/index.tsx
+++ packages/frontend/src/features/posts/List/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import * as useAsync from "@/hooks/useAsync";
 import { UserContext } from "@/context/user";
 import Buttons from "@/components/buttons";
@@ -12,7 +12,13 @@ import styles from "./index.module.css";
 function List() {
     const { user, extract } = useContext(UserContext);
 
-    const [waiting, setWaiting] = useState<boolean>(true);
+    const errorMessageRef = useRef(null);
+    const [errorMessageHeight, setErrorMessageHeight] = useState<number>(0);
+    const createNewPostButtonRef = useRef(null);
+    const [createNewPostButtonHeight, setCreateNewPostButtonHeight] = useState<number>(0);
+
+    const [initialWaiting, setInitialWaiting] = useState(true);
+    const [waiting, setWaiting] = useState(true);
 
     const [posts, setPosts] = useState<Response>([]);
     const [response, setParams, setAttempting, gettingPosts] = useAsync.GET<Params, Response>(
@@ -35,7 +41,9 @@ function List() {
 
     useEffect(() => {
         const newState = response ? response.data : [];
-        setPosts(newState || []);
+        setPosts((currentPosts) => {
+            return currentPosts ? currentPosts.concat(newState || []) : newState || [];
+        });
     }, [response]);
 
     useEffect(() => {
@@ -53,13 +61,19 @@ function List() {
         ]);
     }, [user, extract, setParams, setAttempting]);
 
-    if (response && response.status === 401) window.location.assign("/");
-
     useEffect(() => {
-        if (response && response.status >= 400 && response.message && response.message.length > 0) {
-            setErrorMessage(response.message);
+        if (response) {
+            if (response.status >= 400 && response.message && response.message.length > 0) {
+                setErrorMessage(response.message);
+            } else {
+                setErrorMessage("");
+            }
         }
     }, [response]);
+
+    useEffect(() => {
+        if (!gettingPosts) setInitialWaiting(gettingPosts);
+    }, [gettingPosts]);
 
     useEffect(() => {
         setWaiting(gettingPosts);
@@ -78,13 +92,78 @@ function List() {
         };
     }, []);
 
+    // subscribe to main App component's scroll topic
+    useEffect(() => {
+        PubSub.unsubscribe("page-scroll-reached-bottom");
+        PubSub.subscribe("page-scroll-reached-bottom", () => {
+            if (!waiting && posts) {
+                setAttempting(true);
+                setParams([
+                    {
+                        params: {
+                            userId: extract("_id") as mongoose.Types.ObjectId | undefined | null,
+                            after: posts[posts.length - 1]._id,
+                            repliesOnly: false,
+                        },
+                    },
+                    null,
+                ]);
+            }
+        });
+
+        return () => {
+            PubSub.unsubscribe("page-scroll-reached-bottom");
+        };
+    }, [extract, posts, setAttempting, setParams, waiting]);
+
+    useEffect(() => {
+        let errorMessageRefCurrent: Element;
+        let createNewPostButtonRefCurrent: Element;
+
+        const errorMessageObserver = new ResizeObserver((entries) => {
+            const contentHeight = entries[0].contentRect.height;
+            const padding =
+                parseFloat(window.getComputedStyle(entries[0].target).paddingTop) +
+                parseFloat(window.getComputedStyle(entries[0].target).paddingBottom);
+            const totalHeight = contentHeight + padding;
+            setErrorMessageHeight(totalHeight);
+            PubSub.publish("page-scrollable-area-shift-down", totalHeight);
+        });
+        if (errorMessageRef.current) {
+            errorMessageRefCurrent = errorMessageRef.current;
+            errorMessageObserver.unobserve(errorMessageRef.current);
+            errorMessageObserver.observe(errorMessageRef.current);
+        }
+
+        const createNewPostButtonObserver = new ResizeObserver((entries) => {
+            const contentHeight = entries[0].contentRect.height;
+            const padding =
+                parseFloat(window.getComputedStyle(entries[0].target).paddingTop) +
+                parseFloat(window.getComputedStyle(entries[0].target).paddingBottom);
+            const totalHeight = contentHeight + padding;
+            setCreateNewPostButtonHeight(totalHeight);
+        });
+        if (createNewPostButtonRef.current) {
+            createNewPostButtonRefCurrent = createNewPostButtonRef.current;
+            createNewPostButtonObserver.unobserve(createNewPostButtonRef.current);
+            createNewPostButtonObserver.observe(createNewPostButtonRef.current);
+        }
+
+        return () => {
+            if (errorMessageRefCurrent instanceof Element) {
+                errorMessageObserver.unobserve(errorMessageRefCurrent);
+            }
+            if (createNewPostButtonRefCurrent instanceof Element) {
+                createNewPostButtonObserver.unobserve(createNewPostButtonRefCurrent);
+            }
+        };
+    }, [posts, errorMessage, errorMessageRef, createNewPostButtonRef]);
+
     return (
         <div className={styles["container"]}>
-            {!waiting ? (
+            {!initialWaiting ? (
                 <>
-                    {errorMessage.length > 0 ? (
-                        <p className={styles["error-message"]}>{errorMessage}</p>
-                    ) : null}
+                    <div style={{ height: errorMessageHeight }}></div>
                     {posts && posts.length > 0 ? (
                         posts.map((post) => {
                             return <Posts.Post _id={post._id} canReply key={`post-${post._id}`} />;
@@ -92,20 +171,33 @@ function List() {
                     ) : (
                         <p className={styles["empty-message"]}>Nothing to see here!</p>
                     )}
-                    <div className={styles["create-new-post-button-wrapper"]}>
-                        <div className={styles["create-new-post-button-container"]}>
-                            <Buttons.Basic
-                                text="Create New Post"
-                                symbol="stylus_note"
-                                onClickHandler={() => {
-                                    PubSub.publish("create-new-post-button-click", null);
-                                }}
-                                palette="blue"
-                                otherStyles={{
-                                    fontSize: "1.25rem",
-                                    padding: "0.8rem 1.6rem",
-                                }}
-                            />
+                    <div style={{ height: createNewPostButtonHeight }}></div>
+                    <div className={styles["sticky-wrapper"]}>
+                        <div className={styles["sticky-container"]}>
+                            {errorMessage.length > 0 ? (
+                                <p className={styles["error-message"]} ref={errorMessageRef}>
+                                    {errorMessage}
+                                </p>
+                            ) : (
+                                <p></p>
+                            )}
+                            <div
+                                className={styles["create-new-post-button-container"]}
+                                ref={createNewPostButtonRef}
+                            >
+                                <Buttons.Basic
+                                    text="Create New Post"
+                                    symbol="stylus_note"
+                                    onClickHandler={() => {
+                                        PubSub.publish("create-new-post-button-click", null);
+                                    }}
+                                    palette="blue"
+                                    otherStyles={{
+                                        fontSize: "1.25rem",
+                                        padding: "0.8rem 1.6rem",
+                                    }}
+                                />
+                            </div>
                         </div>
                     </div>
                 </>
