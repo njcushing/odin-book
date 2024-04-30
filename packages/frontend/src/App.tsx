ad6111c0ad71b9a1330d8f61473cef810a5cf30e
njcushing <129661299+njcushing@users.noreply.github.com>
2024-04-30 21:13:44 +0100

    Implement useScrollableElement custom hook in App component
    The component will now publish topics when it is scrolled to the top and bottom of its scrollable area.

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,17 @@
+import { useRef } from "react";
 import Router from "@/routes";
+import useScrollableElement from "./hooks/useScrollableElement";
 
 function App() {
+    const scrollableWrapperRef = useRef<HTMLDivElement>(null);
+    useScrollableElement({
+        ref: scrollableWrapperRef,
+        atTopCallback: () => PubSub.publish("page-scroll-reached-top"),
+        atBottomCallback: () => PubSub.publish("page-scroll-reached-bottom"),
+        isInverted: false,
+        onlyCallbackOnCorrectDirectionalScroll: true,
+    });
+
     return (
         <div
             className="app"
@@ -8,6 +19,7 @@ function App() {
                 placeContent: "center",
                 textAlign: "center",
             }}
+            ref={scrollableWrapperRef}
         >
             <Router />
         </div>
