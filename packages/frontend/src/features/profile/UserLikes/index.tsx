4a05f7b3dd8e566e6951d5a3cd8c19d01380d544
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-01 21:55:49 +0100

    Write functionality for requesting additional user likes when scrolling
    Similarly to other list components, I have updated the layout for the error message.

--- packages/frontend/src/features/profile/UserLikes/index.tsx
+++ packages/frontend/src/features/profile/UserLikes/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import * as useAsync from "@/hooks/useAsync";
 import { ProfileContext } from "@/features/profile/Main";
 import Posts from "@/features/posts";
@@ -9,7 +9,11 @@ import styles from "./index.module.css";
 function UserLikes() {
     const { _id } = useContext(ProfileContext);
 
-    const [waiting, setWaiting] = useState<boolean>(true);
+    const errorMessageRef = useRef(null);
+    const [errorMessageHeight, setErrorMessageHeight] = useState<number>(0);
+
+    const [initialWaiting, setInitialWaiting] = useState(true);
+    const [waiting, setWaiting] = useState(true);
 
     const [likes, setLikes] = useState<Response>([]);
     const [response, setParams, setAttempting, gettingLikes] = useAsync.GET<Params, Response>(
@@ -23,7 +27,9 @@ function UserLikes() {
 
     useEffect(() => {
         const newState = response ? response.data : [];
-        setLikes(newState || []);
+        setLikes((currentLikes) => {
+            return currentLikes ? currentLikes.concat(newState || []) : newState || [];
+        });
     }, [response]);
 
     useEffect(() => {
@@ -32,35 +38,89 @@ function UserLikes() {
         setParams([{ params: { userId: _id, after: null } }, null]);
     }, [_id, setParams, setAttempting]);
 
-    if (response && response.status === 401) window.location.assign("/");
-
     useEffect(() => {
-        if (response && response.status >= 400 && response.message && response.message.length > 0) {
-            setErrorMessage(response.message);
-        } else {
-            setErrorMessage("");
+        if (response) {
+            if (response.status >= 400 && response.message && response.message.length > 0) {
+                setErrorMessage(response.message);
+            } else {
+                setErrorMessage("");
+            }
         }
     }, [response]);
 
     useEffect(() => {
+        if (!gettingLikes) setInitialWaiting(gettingLikes);
         setWaiting(gettingLikes);
     }, [gettingLikes]);
 
-    return !waiting ? (
-        <>
-            {errorMessage.length > 0 ? (
-                <p className={styles["error-message"]}>{errorMessage}</p>
-            ) : null}
-            {likes && likes.length > 0 ? (
-                likes.map((like) => {
-                    return <Posts.Post _id={like._id} key={`post-like-${like._id}`} />;
-                })
+    // subscribe to main App component's scroll topic
+    useEffect(() => {
+        PubSub.unsubscribe("page-scroll-reached-bottom");
+        PubSub.subscribe("page-scroll-reached-bottom", () => {
+            if (!waiting && likes) {
+                setAttempting(true);
+                setParams([{ params: { userId: _id, after: likes[likes.length - 1]._id } }, null]);
+            }
+        });
+
+        return () => {
+            PubSub.unsubscribe("page-scroll-reached-bottom");
+        };
+    }, [likes, _id, setAttempting, setParams, waiting]);
+
+    useEffect(() => {
+        let errorMessageRefCurrent: Element;
+
+        const errorMessageObserver = new ResizeObserver((entries) => {
+            const contentHeight = entries[0].contentRect.height;
+            const padding =
+                parseFloat(window.getComputedStyle(entries[0].target).paddingTop) +
+                parseFloat(window.getComputedStyle(entries[0].target).paddingBottom);
+            const totalHeight = contentHeight + padding;
+            setErrorMessageHeight(totalHeight);
+            PubSub.publish("page-scrollable-area-shift-down", totalHeight);
+        });
+        if (errorMessageRef.current) {
+            errorMessageRefCurrent = errorMessageRef.current;
+            errorMessageObserver.unobserve(errorMessageRef.current);
+            errorMessageObserver.observe(errorMessageRef.current);
+        }
+
+        return () => {
+            if (errorMessageRefCurrent instanceof Element) {
+                errorMessageObserver.unobserve(errorMessageRefCurrent);
+            }
+        };
+    }, [likes, errorMessage, errorMessageRef]);
+
+    return (
+        <div className={styles["container"]}>
+            {!initialWaiting ? (
+                <>
+                    <div style={{ height: errorMessageHeight }}></div>
+                    {likes && likes.length > 0 ? (
+                        likes.map((like) => {
+                            return <Posts.Post _id={like._id} key={`post-like-${like._id}`} />;
+                        })
+                    ) : (
+                        <p className={styles["empty-message"]}>Nothing to see here!</p>
+                    )}
+                    <div className={styles["sticky-wrapper"]}>
+                        <div className={styles["sticky-container"]}>
+                            {errorMessage.length > 0 ? (
+                                <p className={styles["error-message"]} ref={errorMessageRef}>
+                                    {errorMessage}
+                                </p>
+                            ) : (
+                                <p></p>
+                            )}
+                        </div>
+                    </div>
+                </>
             ) : (
-                <p className={styles["empty-message"]}>Nothing to see here!</p>
+                <Accessibility.WaitingWheel />
             )}
-        </>
-    ) : (
-        <Accessibility.WaitingWheel />
+        </div>
     );
 }
 
