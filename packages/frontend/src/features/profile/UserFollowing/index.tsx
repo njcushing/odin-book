505fb7e5379429742f5acac96503020c43c3894c
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-01 22:15:44 +0100

    Write functionality for requesting more users following when scrolling
    Similarly to other list components, I have updated the layout for the error message.

--- packages/frontend/src/features/profile/UserFollowing/index.tsx
+++ packages/frontend/src/features/profile/UserFollowing/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import * as useAsync from "@/hooks/useAsync";
 import { ProfileContext } from "@/features/profile/Main";
 import User from "@/components/user";
@@ -9,6 +9,10 @@ import styles from "./index.module.css";
 function UserFollowing() {
     const { _id } = useContext(ProfileContext);
 
+    const errorMessageRef = useRef(null);
+    const [errorMessageHeight, setErrorMessageHeight] = useState<number>(0);
+
+    const [initialWaiting, setInitialWaiting] = useState(true);
     const [waiting, setWaiting] = useState(true);
 
     const [following, setFollowing] = useState<Response>([]);
@@ -23,7 +27,9 @@ function UserFollowing() {
 
     useEffect(() => {
         const newState = response ? response.data : [];
-        setFollowing(newState || []);
+        setFollowing((currentFollowing) => {
+            return currentFollowing ? currentFollowing.concat(newState || []) : newState || [];
+        });
     }, [response]);
 
     useEffect(() => {
@@ -32,37 +38,100 @@ function UserFollowing() {
         setParams([{ params: { userId: _id, after: null } }, null]);
     }, [_id, setParams, setAttempting]);
 
-    if (response && response.status === 401) window.location.assign("/");
-
     useEffect(() => {
-        if (response && response.status >= 400 && response.message && response.message.length > 0) {
-            setErrorMessage(response.message);
-        } else {
-            setErrorMessage("");
+        if (response) {
+            if (response.status >= 400 && response.message && response.message.length > 0) {
+                setErrorMessage(response.message);
+            } else {
+                setErrorMessage("");
+            }
         }
     }, [response]);
 
     useEffect(() => {
+        if (!gettingFollowing) setInitialWaiting(gettingFollowing);
         setWaiting(gettingFollowing);
     }, [gettingFollowing]);
 
-    return !waiting ? (
-        <>
-            {errorMessage.length > 0 ? (
-                <p className={styles["error-message"]}>{errorMessage}</p>
-            ) : null}
-            {following && following.length > 0 ? (
-                <div className={styles["following"]}>
-                    {following.map((userId) => {
-                        return <User.Option _id={userId} skeleton key={`following-${userId}`} />;
-                    })}
-                </div>
+    // subscribe to main App component's scroll topic
+    useEffect(() => {
+        PubSub.unsubscribe("page-scroll-reached-bottom");
+        PubSub.subscribe("page-scroll-reached-bottom", () => {
+            if (!waiting && following) {
+                setAttempting(true);
+                setParams([
+                    { params: { userId: _id, after: following[following.length - 1] } },
+                    null,
+                ]);
+            }
+        });
+
+        return () => {
+            PubSub.unsubscribe("page-scroll-reached-bottom");
+        };
+    }, [following, _id, setAttempting, setParams, waiting]);
+
+    useEffect(() => {
+        let errorMessageRefCurrent: Element;
+
+        const errorMessageObserver = new ResizeObserver((entries) => {
+            const contentHeight = entries[0].contentRect.height;
+            const padding =
+                parseFloat(window.getComputedStyle(entries[0].target).paddingTop) +
+                parseFloat(window.getComputedStyle(entries[0].target).paddingBottom);
+            const totalHeight = contentHeight + padding;
+            setErrorMessageHeight(totalHeight);
+            PubSub.publish("page-scrollable-area-shift-down", totalHeight);
+        });
+        if (errorMessageRef.current) {
+            errorMessageRefCurrent = errorMessageRef.current;
+            errorMessageObserver.unobserve(errorMessageRef.current);
+            errorMessageObserver.observe(errorMessageRef.current);
+        }
+
+        return () => {
+            if (errorMessageRefCurrent instanceof Element) {
+                errorMessageObserver.unobserve(errorMessageRefCurrent);
+            }
+        };
+    }, [following, errorMessage, errorMessageRef]);
+
+    return (
+        <div className={styles["container"]}>
+            {!initialWaiting ? (
+                <>
+                    <div style={{ height: errorMessageHeight }}></div>
+                    {following && following.length > 0 ? (
+                        <div className={styles["following"]}>
+                            {following.map((userId) => {
+                                return (
+                                    <User.Option
+                                        _id={userId}
+                                        skeleton
+                                        key={`following-${userId}`}
+                                    />
+                                );
+                            })}
+                        </div>
+                    ) : (
+                        <p className={styles["empty-message"]}>Nothing to see here!</p>
+                    )}
+                    <div className={styles["sticky-wrapper"]}>
+                        <div className={styles["sticky-container"]}>
+                            {errorMessage.length > 0 ? (
+                                <p className={styles["error-message"]} ref={errorMessageRef}>
+                                    {errorMessage}
+                                </p>
+                            ) : (
+                                <p></p>
+                            )}
+                        </div>
+                    </div>
+                </>
             ) : (
-                <p className={styles["empty-message"]}>Nothing to see here!</p>
+                <Accessibility.WaitingWheel />
             )}
-        </>
-    ) : (
-        <Accessibility.WaitingWheel />
+        </div>
     );
 }
 
