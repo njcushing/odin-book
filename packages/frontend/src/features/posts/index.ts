f22114b691e9f48945872cb8c0aacde250fbe6c0
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-03 09:36:41 +0100

    Export post Replies component through main post-component index file

--- packages/frontend/src/features/posts/index.ts
+++ packages/frontend/src/features/posts/index.ts
@@ -1,10 +1,12 @@
 import Post from "./Post";
+import Replies from "./Replies";
 import List from "./List";
 import Likes from "./Likes";
 import Create from "./Create";
 
 const Posts = {
     Post,
+    Replies,
     List,
     Likes,
     Create,
