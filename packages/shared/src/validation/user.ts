837f72f17b0c1bb72177d8e1a81a5d8162c175a6
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-03 10:44:05 +0100

    Write validators for user's profileImage

--- packages/shared/src/validation/user.ts
+++ packages/shared/src/validation/user.ts
@@ -1,3 +1,8 @@
+import * as extendedTypes from "@shared/utils/extendedTypes";
+import isStringBase64 from "@shared/utils/isStringBase64";
+
+const maximumImageSize = 2000000;
+
 type ReturnTypes = {
     status: boolean;
     message: string;
@@ -153,5 +158,61 @@ export const bio = (value: string, messageType: "front" | "back"): ReturnTypes =
     return {
         status: true,
         message: messageType === "front" ? "Valid Bio." : "'bio' field (String) is valid",
+    };
+};
+
+export const imageArray = (
+    value: extendedTypes.TypedArray,
+    messageType: "front" | "back",
+): ReturnTypes => {
+    if (!extendedTypes.isTypedArray(value)) {
+        return {
+            status: false,
+            message:
+                messageType === "front"
+                    ? `Post image must be a Typed Array.`
+                    : `'image' field must be a Typed Array.`,
+        };
+    }
+    if (value.length > maximumImageSize) {
+        return {
+            status: false,
+            message:
+                messageType === "front"
+                    ? `Post image must be smaller than 2MB.`
+                    : `'image' field (Array) must be smaller than 2MB.`,
+        };
+    }
+    return {
+        status: true,
+        message: messageType === "front" ? `Valid post image.` : `'image' field (Array) is valid`,
+    };
+};
+
+export const imageBase64 = (value: string, messageType: "front" | "back"): ReturnTypes => {
+    const metadataRemoved = value.slice(value.indexOf(",") + 1);
+    if (!isStringBase64(metadataRemoved)) {
+        return {
+            status: false,
+            message:
+                messageType === "front"
+                    ? `Chat image must be a base64-encoded string.`
+                    : `'image' field must be a base64-encoded string.`,
+        };
+    }
+    const binary = atob(metadataRemoved);
+    const fileSize = binary.length;
+    if (fileSize > maximumImageSize) {
+        return {
+            status: false,
+            message:
+                messageType === "front"
+                    ? `Chat image must be smaller than 2MB.`
+                    : `'image' field (Array) must be smaller than 2MB.`,
+        };
+    }
+    return {
+        status: true,
+        message: messageType === "front" ? `Valid chat image.` : `'image' field (string) is valid`,
     };
 };
