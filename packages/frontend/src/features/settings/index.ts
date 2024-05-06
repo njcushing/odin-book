553507ba93794b284fd992a21631a7eae0c8cfbe
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-06 11:09:01 +0100

    Include Profile component in settings components' export file

--- packages/frontend/src/features/settings/index.ts
+++ packages/frontend/src/features/settings/index.ts
@@ -1,11 +1,13 @@
 import Main, { routes as MainRoutes } from "./Main";
 import List from "./List";
 import FieldUpdater from "./FieldUpdater";
+import Profile from "./Profile";
 
 const Settings = {
     Main,
     List,
     FieldUpdater,
+    Profile,
 };
 
 export default Settings;
