2317a65d56c8799d515c44e822b876880d2a02f5
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-06 12:21:28 +0100

    Create Image input component

---
+++ packages/frontend/src/components/inputs/components/Image/index.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import * as extendedTypes from "@shared/utils/extendedTypes";
+import Images from "@/components/images";
+import Buttons from "@/components/buttons";
+import convertArrayBufferToBase64 from "@/utils/convertArrayBufferToBase64";
+import * as Types from "../../types";
+import validateUploadedFile from "../File/utils/validateUploadedFile";
+import Inputs from "../..";
+import styles from "./index.module.css";
+
+type AcceptedImageTypes = extendedTypes.TypedArray | string;
+
+export type ImageInfo = {
+    data: AcceptedImageTypes;
+    file: File | null;
+} | null;
+
+type Custom = {
+    description?: string;
+    onChangeHandler?: ((imageBase64: string | null) => void) | null;
+    imageSizePx?: number;
+};
+
+export type TImage = Types.Base<AcceptedImageTypes> &
+    Types.Error &
+    Types.Validator<extendedTypes.TypedArray> &
+    Types.Sizes &
+    Custom;
+
+function Image({
+    labelText,
+    fieldId,
+    fieldName,
+    initialValue = "",
+    disabled = false,
+    required = false,
+    errorMessage = "",
+    validator = null,
+    size = "s",
+    description = "",
+    onChangeHandler = null,
+    imageSizePx = 64,
+}: TImage) {
+    const [value, setValue] = useState<ImageInfo>({ data: initialValue, file: null });
+    const [error, setError] = useState<string>("");
+
+    let currentErrorMessage = "";
+    if (errorMessage && errorMessage.length > 0) {
+        currentErrorMessage = errorMessage;
+    } else if (error && error.length > 0) {
+        currentErrorMessage = error;
+    }
+
+    return (
+        <div className={styles["wrapper"]}>
+            <Inputs.Label labelText={labelText} fieldId={fieldId} required={required} size={size} />
+            <Inputs.Description text={description} size={size} />
+            <div className={styles["image-and-upload-button-container"]}>
+                <div className={styles["test"]}>
+                    <Images.Profile src={value ? value.data : ""} sizePx={imageSizePx} />
+                </div>
+                <Buttons.Upload
+                    labelText=""
+                    fieldId={fieldId}
+                    fieldName={fieldName}
+                    accept="image.*"
+                    disabled={disabled}
+                    button={{
+                        symbol: "edit",
+                        palette: "primary",
+                        style: { shape: "rounded" },
+                        otherStyles: { fontSize: "1.4rem", padding: "0.4rem" },
+                    }}
+                    onUploadHandler={async (uploads: [ProgressEvent<FileReader>, File][]) => {
+                        const upload = uploads[0];
+                        const [status, message, data] = validateUploadedFile(
+                            upload,
+                            "image.*",
+                            validator,
+                        );
+                        setError(!status ? message : "");
+                        const newImage = status && data ? data : null;
+                        setValue(newImage);
+                        const imageBase64 = newImage
+                            ? await convertArrayBufferToBase64(newImage.data)
+                            : null;
+                        if (onChangeHandler) onChangeHandler(imageBase64);
+                    }}
+                />
+            </div>
+            {!disabled && currentErrorMessage.length > 0 ? (
+                <Inputs.Error text={currentErrorMessage} size={size} />
+            ) : null}
+        </div>
+    );
+}
+
+export default Image;
