aab50adee414fd0e5858bcc6dc1ee5b57de9033b
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-08 20:41:21 +0100

    Include Preferences component in settings components' export file

--- packages/frontend/src/features/settings/index.ts
+++ packages/frontend/src/features/settings/index.ts
@@ -2,12 +2,14 @@ import Main, { routes as MainRoutes } from "./Main";
 import List from "./List";
 import FieldUpdater from "./FieldUpdater";
 import Profile from "./Profile";
+import Preferences from "./Preferences";
 
 const Settings = {
     Main,
     List,
     FieldUpdater,
     Profile,
+    Preferences,
 };
 
 export default Settings;
