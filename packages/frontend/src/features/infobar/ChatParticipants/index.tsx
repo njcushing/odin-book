4cb7b23c6b8f8bed6b9d2062ae2adfc0940de9d3
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-09 11:47:07 +0100

    Create ChatParticipants infobar component

---
+++ packages/frontend/src/features/infobar/ChatParticipants/index.tsx
@@ -0,0 +1,54 @@
+import Images from "@/components/images";
+import { ReturnTypes } from "@/features/chat/Active/utils/extractParticipantsInformation";
+import styles from "./index.module.css";
+
+export type TChatParticipants = {
+    participants: ReturnTypes;
+};
+
+const roleHeirarchy = ["admin", "moderator", "guest"];
+
+const organiseParticipants = (participants: ReturnTypes) => {
+    const participantsArr = Object.values(participants);
+    participantsArr.sort((a, b) => roleHeirarchy.indexOf(a.role) - roleHeirarchy.indexOf(b.role));
+    return participantsArr;
+};
+
+function ChatParticipants({ participants }: TChatParticipants) {
+    return (
+        <div className={styles["container"]}>
+            <h4 className={styles["title"]}>Participants</h4>
+            <div className={styles["scrollable-wrapper"]}>
+                <ul className={styles["participants"]}>
+                    {organiseParticipants(participants).map((participant) => {
+                        let role;
+                        if (participant.role === "admin") role = "star";
+                        if (participant.role === "moderator") role = "shield";
+
+                        return (
+                            <div
+                                className={styles["participant-container"]}
+                                key={`infobar-participant-${participant.userId}`}
+                            >
+                                <Images.Profile sizePx={48} />
+                                <p className={styles["name"]}>{participant.inChatName}</p>
+                                <ul className={styles["symbols"]}>
+                                    {role ? (
+                                        <p
+                                            className={`material-symbols-rounded no-select ${styles["role"]}`}
+                                            aria-label={participant.role}
+                                        >
+                                            {role}
+                                        </p>
+                                    ) : null}
+                                </ul>
+                            </div>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default ChatParticipants;
