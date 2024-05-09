e481d3c16f94af225b90cfb924b4030b17759f0e
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-09 12:11:11 +0100

    Remove LayoutUI implementation from Chats page component

--- packages/frontend/src/pages/Chats/index.tsx
+++ packages/frontend/src/pages/Chats/index.tsx
@@ -1,7 +1,5 @@
 import { Outlet } from "react-router-dom";
-import LayoutUI from "@/layouts";
 import Chat from "@/features/chat";
-import Infobar from "@/features/infobar";
 import styles from "./index.module.css";
 
 export const routes = [
@@ -18,81 +16,11 @@ export const routes = [
 ];
 
 function Chats() {
-    const layout = (
-        <LayoutUI.Spatial
-            width="100%"
-            height="auto"
-            arrangements={[
-                {
-                    type: "columns",
-                    minWidth: 920,
-                    maxWidth: 999999,
-                    minHeight: 0,
-                    maxHeight: 999999,
-                    areas: [
-                        { size: "600px", children: [<Outlet key={0} />] },
-                        { size: "320px", children: [<Infobar.Wrapper key={0} />] },
-                    ],
-                    style: {
-                        justifySelf: "flex-start",
-                        alignSelf: "flex-start",
-                        width: "1200px",
-                        height: "auto",
-                        padding: "0rem",
-                    },
-                },
-                {
-                    type: "columns",
-                    minWidth: 600,
-                    maxWidth: 920,
-                    minHeight: 0,
-                    maxHeight: 999999,
-                    areas: [{ size: "600px", children: [<Outlet key={0} />] }],
-                    style: {
-                        justifySelf: "flex-start",
-                        alignSelf: "flex-start",
-                        width: "660px",
-                        height: "auto",
-                        padding: "0rem",
-                    },
-                },
-                {
-                    type: "columns",
-                    minWidth: 300,
-                    maxWidth: 600,
-                    minHeight: 0,
-                    maxHeight: 999999,
-                    areas: [{ size: "300px", children: [<Outlet key={0} />] }],
-                    style: {
-                        justifySelf: "flex-start",
-                        alignSelf: "flex-start",
-                        width: "360px",
-                        height: "auto",
-                        padding: "0rem",
-                    },
-                },
-                {
-                    type: "columns",
-                    minWidth: 0,
-                    maxWidth: 360,
-                    minHeight: 0,
-                    maxHeight: 999999,
-                    areas: [{ size: "1fr", children: [<Outlet key={0} />] }],
-                    style: {
-                        justifySelf: "flex-start",
-                        alignSelf: "flex-start",
-                        width: "100%",
-                        height: "auto",
-                        padding: "0rem",
-                    },
-                },
-            ]}
-        />
-    );
-
     return (
         <div className={styles["wrapper"]}>
-            <div className={styles["container"]}>{layout}</div>
+            <div className={styles["container"]}>
+                <Outlet />
+            </div>
         </div>
     );
 }
