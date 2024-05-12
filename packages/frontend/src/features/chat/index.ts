7d29caa7631c65bb65a6cedeb6f757242d65cf39
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-12 12:13:32 +0100

    Include chat Image component in chat components' export file

--- packages/frontend/src/features/chat/index.ts
+++ packages/frontend/src/features/chat/index.ts
@@ -2,6 +2,7 @@ import Header from "./Header";
 import List from "./List";
 import Option from "./Option";
 import Active from "./Active";
+import Image from "./Image";
 import MessageList from "./MessageList";
 import Message from "./Message";
 import MessageInput from "./MessageInput";
@@ -14,6 +15,7 @@ const Chat = {
     List,
     Option,
     Active,
+    Image,
     MessageList,
     Message,
     MessageInput,
