859dd264694102fbb655e5e26d2a47fb067f7f3a
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-12 11:00:33 +0100

    Write function to request user's recent chat activity from API

---
+++ packages/frontend/src/features/infobar/RecentChatActivity/utils/getRecentChatActivity.ts
@@ -0,0 +1,99 @@
+import * as apiFunctionTypes from "@shared/utils/apiFunctionTypes";
+import mongoose from "mongoose";
+import saveTokenFromAPIResponse from "@/utils/saveTokenFromAPIResponse";
+
+export type Params = {
+    userId: mongoose.Types.ObjectId | null | undefined;
+};
+
+export type Response =
+    | {
+          _id: mongoose.Types.ObjectId;
+          createdBy: mongoose.Types.ObjectId;
+          participants: {
+              user: {
+                  _id: mongoose.Types.ObjectId;
+                  accountTag: string;
+                  preferences: {
+                      displayName: string;
+                      profileImage: {
+                          _id: mongoose.Types.ObjectId;
+                          url: string;
+                          alt: string;
+                      } | null;
+                  };
+              };
+              nickname: string;
+              role: "admin" | "moderator" | "guest";
+              muted: boolean;
+          }[];
+          name: string;
+          image: {
+              _id: mongoose.Types.ObjectId;
+              url: string;
+              alt: string;
+          } | null;
+          recentMessage: {
+              _id: mongoose.Types.ObjectId;
+              author: mongoose.Types.ObjectId;
+              text: string;
+              imageCount: number;
+              deleted: boolean;
+          } | null;
+      }[]
+    | null;
+
+const getRecentChatActivity: apiFunctionTypes.GET<Params, Response> = async (
+    data,
+    abortController = null,
+) => {
+    const { userId } = data.params as Params;
+
+    if (!userId) {
+        return {
+            status: 400,
+            message: "No userId provided to route path",
+            data: null,
+        };
+    }
+
+    const queryObject = { limit: "3" };
+    const urlParams = new URLSearchParams();
+    Object.entries(queryObject).forEach(([key, value]) => {
+        if (value !== "" && value !== undefined && value !== null) {
+            urlParams.append(key, `${value}`);
+        }
+    });
+
+    const result = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/user/${userId}/chat-activity?${urlParams}`,
+        {
+            signal: abortController ? abortController.signal : null,
+            method: "GET",
+            mode: "cors",
+            headers: {
+                Authorization: localStorage.getItem("odin-book-auth-token") || "",
+            },
+        },
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                data: responseJSON.data.chatActivity,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+                data: null,
+            };
+        });
+    return result;
+};
+
+export default getRecentChatActivity;
