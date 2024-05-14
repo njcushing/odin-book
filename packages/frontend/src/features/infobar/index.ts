f385b19d175bdae02f9b39b702a1e8c4b4673bd5
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-14 20:46:37 +0100

    Include RecentChatActivity component in export file

--- packages/frontend/src/features/infobar/index.ts
+++ packages/frontend/src/features/infobar/index.ts
@@ -1,10 +1,12 @@
 import Wrapper from "./Wrapper";
 import RecommendedUsers from "./RecommendedUsers";
+import RecentChatActivity from "./RecentChatActivity";
 import ChatParticipants from "./ChatParticipants";
 
 const Infobar = {
     Wrapper,
     RecommendedUsers,
+    RecentChatActivity,
     ChatParticipants,
 };
 
