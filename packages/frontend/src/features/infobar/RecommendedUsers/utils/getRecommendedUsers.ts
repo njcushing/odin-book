3eabeb8efe519178e31469f56c9b02a5bfaff527
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-16 12:20:21 +0100

    Write function to request recommended users from API

---
+++ packages/frontend/src/features/infobar/RecommendedUsers/utils/getRecommendedUsers.ts
@@ -0,0 +1,64 @@
+import * as apiFunctionTypes from "@shared/utils/apiFunctionTypes";
+import mongoose from "mongoose";
+import saveTokenFromAPIResponse from "@/utils/saveTokenFromAPIResponse";
+
+export type Params = {
+    userId: mongoose.Types.ObjectId | null | undefined;
+};
+
+export type Response = mongoose.Types.ObjectId[] | null;
+
+const getRecommendedUsers: apiFunctionTypes.GET<Params, Response> = async (
+    data,
+    abortController = null,
+) => {
+    const { userId } = data.params as Params;
+
+    if (!userId) {
+        return {
+            status: 400,
+            message: "No userId provided to route path",
+            data: null,
+        };
+    }
+
+    const queryObject = { limit: "3" };
+    const urlParams = new URLSearchParams();
+    Object.entries(queryObject).forEach(([key, value]) => {
+        if (value !== "" && value !== undefined && value !== null) {
+            urlParams.append(key, `${value}`);
+        }
+    });
+
+    const result = await fetch(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/user/${userId}/recommended-users?${urlParams}`,
+        {
+            signal: abortController ? abortController.signal : null,
+            method: "GET",
+            mode: "cors",
+            headers: {
+                Authorization: localStorage.getItem("odin-book-auth-token") || "",
+            },
+        },
+    )
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                data: responseJSON.data.recommendedUsers,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+                data: null,
+            };
+        });
+    return result;
+};
+
+export default getRecommendedUsers;
