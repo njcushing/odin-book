55d3c5983627ff9c856a039106d69d08f6e91083
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-16 11:39:09 +0100

    Include RecentPosts component in post components' export file

--- packages/frontend/src/features/infobar/index.ts
+++ packages/frontend/src/features/infobar/index.ts
@@ -1,11 +1,13 @@
 import Wrapper from "./Wrapper";
 import RecommendedUsers from "./RecommendedUsers";
+import RecentPosts from "./RecentPosts";
 import RecentChatActivity from "./RecentChatActivity";
 import ChatParticipants from "./ChatParticipants";
 
 const Infobar = {
     Wrapper,
     RecommendedUsers,
+    RecentPosts,
     RecentChatActivity,
     ChatParticipants,
 };
