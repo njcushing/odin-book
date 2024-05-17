e190341a81aa7bc2ff0e524b6c38927260d2a597
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-17 10:19:10 +0100

    Fix date day suffix calculation

--- packages/frontend/src/utils/formatCreationDate.ts
+++ packages/frontend/src/utils/formatCreationDate.ts
@@ -8,18 +8,21 @@ const formatCreationDate = (date: string): string => {
     const { year } = inputDate;
 
     let formattedDay;
-    if ((day > 3 && day < 21) || day > 23) formattedDay = `${day}th`;
-    switch (day % 10) {
-        case 1:
-            formattedDay = `${day}st`;
-            break;
-        case 2:
-            formattedDay = `${day}nd`;
-            break;
-        case 3:
-            formattedDay = `${day}rd`;
-            break;
-        default:
+    if ((day > 3 && day < 21) || day > 23) {
+        formattedDay = `${day}th`;
+    } else {
+        switch (day % 10) {
+            case 1:
+                formattedDay = `${day}st`;
+                break;
+            case 2:
+                formattedDay = `${day}nd`;
+                break;
+            case 3:
+                formattedDay = `${day}rd`;
+                break;
+            default:
+        }
     }
 
     return `Joined ${formattedDay} ${month}, ${year}`;
