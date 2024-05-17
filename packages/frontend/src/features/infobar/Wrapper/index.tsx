1d00df4fc774612fffca0d2947f65bcd926c0670
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-17 11:53:10 +0100

    Update infobar Wrapper component to be customisable
    The chosen panels to display in the infobar in addition to any children can now be set using the PubSub package. The reason I have done this is to allow for the sidebar to change from anywhere in the component heirarchy while mounting the Wrapper component itself at a much higher level. Before, the Wrapper component was being used in various nested components that, when mounted/unmounted, would trigger a rerender in the sidebar - I wanted the sidebar to change without rerendering panels that stayed consistent between different pages, and this method achieves this.

--- packages/frontend/src/features/infobar/Wrapper/index.tsx
+++ packages/frontend/src/features/infobar/Wrapper/index.tsx
@@ -1,21 +1,26 @@
 import React, { useState, useEffect, useRef } from "react";
+import RecommendedUsers from "../RecommendedUsers";
+import RecentPosts from "../RecentPosts";
+import RecentChatActivity from "../RecentChatActivity";
 import styles from "./index.module.css";
 
 type TWrapper = {
+    initialChoices?: string[];
     style?: React.CSSProperties;
     children?: React.ReactNode;
 };
 
-function Wrapper({ style, children }: TWrapper) {
+function Wrapper({ initialChoices = ["RecommendedUsers"], style, children }: TWrapper) {
+    const [choices, setChoices] = useState<string[] | null>(initialChoices || null);
+    const [childrenState, setChildrenState] = useState<React.ReactNode | null>(children || null);
+
+    // handle layout
     const [wrapperHeight, setWrapperHeight] = useState<number>(0);
-
     const wrapperRef = useRef<HTMLDivElement>(null);
-
     useEffect(() => {
         if (wrapperRef.current) setWrapperHeight(wrapperRef.current.clientHeight);
         else setWrapperHeight(0);
     }, [wrapperRef, wrapperHeight]);
-
     useEffect(() => {
         let wrapperRefCurrent: Element;
         const observer = new ResizeObserver((entries) => {
@@ -29,14 +34,42 @@ function Wrapper({ style, children }: TWrapper) {
             if (wrapperRefCurrent instanceof Element) observer.unobserve(wrapperRefCurrent);
         };
     }, []);
-
     const wrapperTop = `min(0px, calc(100vh - ${wrapperHeight}px))`;
 
-    return (
+    // subscribe to topics for customising sidebar
+    useEffect(() => {
+        PubSub.unsubscribe("sidebar-set-choices");
+        PubSub.unsubscribe("sidebar-set-children");
+
+        PubSub.subscribe("sidebar-set-choices", (msg, data) => {
+            setChoices(data);
+        });
+        PubSub.subscribe("sidebar-set-children", (msg, data) => {
+            setChildrenState(data);
+        });
+
+        return () => {
+            PubSub.unsubscribe("sidebar-set-choices");
+            PubSub.unsubscribe("sidebar-set-children");
+        };
+    }, []);
+
+    // assign correct components based on choices
+    const chosenElements = [];
+    if (choices) {
+        if (choices.includes("RecommendedUsers")) chosenElements.push(<RecommendedUsers />);
+        if (choices.includes("RecentPosts")) chosenElements.push(<RecentPosts />);
+        if (choices.includes("RecentChatActivity")) chosenElements.push(<RecentChatActivity />);
+    }
+
+    return (choices && choices.length > 0) || childrenState ? (
         <div className={styles["wrapper"]} ref={wrapperRef} style={{ ...style, top: wrapperTop }}>
-            <div className={styles["container"]}>{children}</div>
+            <div className={styles["container"]}>
+                {chosenElements}
+                {children}
+            </div>
         </div>
-    );
+    ) : null;
 }
 
 export default Wrapper;
