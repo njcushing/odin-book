0c13cb9771ca9780b94bcf9e99bf1010531fdbaa
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-17 10:28:08 +0100

    Do not display <img> element when src prop not provided

--- packages/frontend/src/components/images/components/Basic/index.tsx
+++ packages/frontend/src/components/images/components/Basic/index.tsx
@@ -4,7 +4,7 @@ import styles from "./index.module.css";
 
 export type BasicTypes = Types.Base;
 
-function Basic({ src = "", alt = "", label = "image", style }: BasicTypes) {
+function Basic({ src, alt = "", label = "image", style }: BasicTypes) {
     let imgSrc = "";
     if (src) {
         if (typeof src === "string") {
@@ -14,9 +14,17 @@ function Basic({ src = "", alt = "", label = "image", style }: BasicTypes) {
         }
     }
 
+    const defaultStyles = !src
+        ? {
+              backgroundColor: "var(--background-tertiary)",
+          }
+        : {};
+
     return (
-        <div className={styles["container"]} style={{ ...style }}>
-            <img className={styles["image"]} aria-label={label} src={imgSrc} alt={alt}></img>
+        <div className={styles["container"]} style={{ ...style, ...defaultStyles }}>
+            {imgSrc.length > 0 && (
+                <img className={styles["image"]} aria-label={label} src={imgSrc} alt={alt}></img>
+            )}
         </div>
     );
 }
