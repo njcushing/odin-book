ef388235ac7fecea5059067923ece3e9c3a1b05c
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-19 13:38:59 +0100

    Write function for fetching all users from API

---
+++ packages/frontend/src/features/users/List/utils/getAllUsers.ts
@@ -0,0 +1,55 @@
+import * as apiFunctionTypes from "@shared/utils/apiFunctionTypes";
+import mongoose from "mongoose";
+import saveTokenFromAPIResponse from "@/utils/saveTokenFromAPIResponse";
+
+export type Params = {
+    excludeActiveUser?: boolean;
+    after?: mongoose.Types.ObjectId | null | undefined;
+    limit?: number;
+};
+
+export type Response = mongoose.Types.ObjectId[] | null;
+
+const getAllUsers: apiFunctionTypes.GET<Params, Response> = async (
+    data,
+    abortController = null,
+) => {
+    const { excludeActiveUser, after, limit = "10" } = data.params as Params;
+
+    const queryObject = { excludeActiveUser, after, limit };
+    const urlParams = new URLSearchParams();
+    Object.entries(queryObject).forEach(([key, value]) => {
+        if (value !== "" && value !== undefined && value !== null) {
+            urlParams.append(key, `${value}`);
+        }
+    });
+
+    const result = await fetch(`${import.meta.env.VITE_SERVER_DOMAIN}/users/all?${urlParams}`, {
+        signal: abortController ? abortController.signal : null,
+        method: "GET",
+        mode: "cors",
+        headers: {
+            Authorization: localStorage.getItem("odin-book-auth-token") || "",
+        },
+    })
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                data: responseJSON.data ? responseJSON.data.users : null,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+                data: null,
+            };
+        });
+    return result;
+};
+
+export default getAllUsers;
