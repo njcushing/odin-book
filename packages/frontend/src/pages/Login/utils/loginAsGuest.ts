26770afb6d46864a0489e1797b323bf7c68ad27d
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-21 20:48:17 +0100

    Write function for requesting login as a guest

---
+++ packages/frontend/src/pages/Login/utils/loginAsGuest.ts
@@ -0,0 +1,37 @@
+import * as apiFunctionTypes from "@shared/utils/apiFunctionTypes";
+import saveTokenFromAPIResponse from "@/utils/saveTokenFromAPIResponse";
+
+export type Params = null;
+
+export type Response = null;
+
+const getAllUsers: apiFunctionTypes.GET<Params, Response> = async (
+    data,
+    abortController = null,
+) => {
+    const result = await fetch(`${import.meta.env.VITE_SERVER_DOMAIN}/auth/login-as-guest`, {
+        signal: abortController ? abortController.signal : null,
+        method: "GET",
+        mode: "cors",
+    })
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                data: null,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+                data: null,
+            };
+        });
+    return result;
+};
+
+export default getAllUsers;
