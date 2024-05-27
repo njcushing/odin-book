50182ffce1cd6bad82e61c0e22040d7fbe735c81
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-27 12:54:10 +0100

    Delete validateMongoDBObjectId function as it is no longer in use

--- packages/frontend/src/utils/validateMongoDBObjectId.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import * as extendedTypes from "@shared/utils/extendedTypes";
-
-const validateMongoDBObjectId = (id: extendedTypes.MongoDBObjectId): boolean => {
-    return mongoose.Types.ObjectId.isValid(id);
-};
-
-export default validateMongoDBObjectId;
