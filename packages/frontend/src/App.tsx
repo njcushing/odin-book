5e6c1d360cefd524559c659db1865cc7b893e860
njcushing <129661299+njcushing@users.noreply.github.com>
2024-05-30 09:51:22 +0100

    Write function to set window inner width and height units as global vars

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -47,6 +47,23 @@ function App() {
     if (localStorage.getItem("odin-book-theme") === null) saveTheme("default");
     loadTheme();
 
+    // calculate vw and vh units
+    useEffect(() => {
+        const setUnits = () => {
+            const vw = window.innerWidth * 0.01;
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty("--vw", `${vw}px`);
+            document.documentElement.style.setProperty("--vh", `${vh}px`);
+        };
+
+        setUnits();
+        window.addEventListener("resize", setUnits);
+
+        return () => {
+            window.removeEventListener("resize", setUnits);
+        };
+    }, []);
+
     return (
         <AppContext.Provider value={useMemo(() => ({ isScrollable }), [isScrollable])} key={0}>
             <div
